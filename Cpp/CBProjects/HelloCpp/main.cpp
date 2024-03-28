a8040d682fa6c5e244ac8fb94c1f5733753d5d29
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-03-28 11:14:34 +0530

    added code to prevent terminal closing on itself

--- Cpp/CBProjects/HelloCpp/main.cpp
+++ Cpp/CBProjects/HelloCpp/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
     std::cout << "Hello C++";
+    std::cin.clear(); // resets any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        // ignore any characters in the input line buffer until we find a newline
+    std::cin.get();
+        // literally means get one more character from user
+        // in practice waits for user to press enter
+        // added these lines so the terminal windows doesn't closes.
     return 0;
 }
