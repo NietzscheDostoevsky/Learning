6d4699310c75226e8f66feba9cfd987c16e4bd43
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-03-28 19:23:20 +0530

    needs xterm to run

--- Cpp/Linux/CBProjectsLinux/HelloCpp/main.cpp
+++ Cpp/Linux/CBProjectsLinux/HelloCpp/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main()
 {
     std::cout << "Hello C++" ;
+    std::cin.clear(); // reset any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find a newline
+    std::cin.get(); // get one more char from the user (waits for user to press enter)
+
     return 0;
 }
