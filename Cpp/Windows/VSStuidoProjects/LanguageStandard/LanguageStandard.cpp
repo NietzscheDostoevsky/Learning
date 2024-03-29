d15f0f44774809f92f85d69dc2672a0a6d06efc9
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-03-29 12:20:39 +0530

    update source file

--- Cpp/Windows/VSStuidoProjects/LanguageStandard/LanguageStandard.cpp
+++ Cpp/Windows/VSStuidoProjects/LanguageStandard/LanguageStandard.cpp
@@ -1,20 +1,49 @@
-// LanguageStandard.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+const int numStandards = 7;
+const long stdCode[numStandards] = { 199711L, 201103L, 201402L, 201703L, 202002L, 202302L, 202612L };
+const char* stdName[numStandards] = { "Pre-C++11", "C++11", "C++14", "C++17", "C++20", "C++23", "C++26" };
+
+long getCPPStandard()
+{
+    // Visual Studio is non-conforming in support for __cplusplus (unless you set a specific compiler flag, which you probably haven't)
+    // In Visual Studio 2015 or newer we can use _MSVC_LANG instead
+    // See https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
+#if defined (_MSVC_LANG)
+    return _MSVC_LANG;
+#elif defined (_MSC_VER)
+    // If we're using an older version of Visual Studio, bail out
+    return 1;
+#else
+    // __cplusplus is the intended way to query the language standard code (as defined by the language standards)
+    return __cplusplus;
+#endif
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    long standard = getCPPStandard();
+
+    if (standard == 1)
+    {
+        std::cout << "Error: Unable to determine your language standard.  Sorry.\n";
+        return 0;
+    }
+
+    std::cout << "Your compiler is using language standard: ";
+    for (int i = 0; i < numStandards; ++i)
+    {
+        if (standard <= stdCode[i])
+        {
+            std::cout << stdName[i];
+            // If the reported version is between two standard codes, this must be a preview / experimental support
+            if (standard < stdCode[i])
+                std::cout << " (preview)";
+            break;
+        }
+    }
+
+    std::cout << '\n';
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
