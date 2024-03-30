79a1a1c5c4e785f5cd4e84db2be66d0b42d147a0
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-03-30 13:27:48 +0530

    suits and cards

--- FluentPython/Chapter01/CardDeck.py
+++ FluentPython/Chapter01/CardDeck.py
@@ -13,3 +13,8 @@ class FrenchDeck:
         return len(self._cards)
     def __getitem__(self, position):
         return self._cards[position]
+
+ranks = [str(n) for n in range(2,11)] + list('JQKA')
+suits = 'spades dismonds clubs hearts'.split()
+cards = [Card(rank, suit) for suit in suits
+                          for rank in ranks] 
