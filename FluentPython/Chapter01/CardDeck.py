d2b2dd47675f438c7eeeb8f129f3b6d26e38e73a
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-03-31 21:46:38 +0530

    Update CardDeck.py

--- FluentPython/Chapter01/CardDeck.py
+++ FluentPython/Chapter01/CardDeck.py
@@ -18,3 +18,7 @@ ranks = [str(n) for n in range(2,11)] + list('JQKA')
 suits = 'spades dismonds clubs hearts'.split()
 cards = [Card(rank, suit) for suit in suits
                           for rank in ranks] 
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
