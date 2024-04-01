b42bf464b0f340a43fccfc511985e7e5cb4a2150
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-04-01 23:31:56 +0530

    entered one and many rows

--- SQL/Project/PythonIntegration/tutorial_enterdata.py
+++ SQL/Project/PythonIntegration/tutorial_enterdata.py
@@ -8,7 +8,21 @@ c = conn.cursor()
 
 
 
+c.execute("INSERT INTO CUSTOMERS VALUES('SUNDARA', 'SUNDARI', 'SUNDARA@GMAIL.COM')")
+
 conn.commit()
 
 # Close the connection 
+
+
+# Many records
+
+many_customers = [('Macha', 'Machi', 'machamachi@gmail.com'),
+                 ('Gulto', 'Gulti', 'gultotulti@gmailc.comm'),
+                 ('Mahi', 'Dhoni', 'mahi@dhoni.com')]
+
+c.executemany("INSERT INTO CUSTOMERS VALUES (?,?,?)", many_customers)
+conn.commit()
+
+print("Command executed successfully...")
 conn.close()
