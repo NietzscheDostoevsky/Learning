fae79c6d412d889832761922d9d6ca2ffdf83294
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-04-02 02:39:50 +0530

    added doc

---
+++ SQL/PosgreSQLPython/suppliers/create_tables.py
@@ -0,0 +1,12 @@
+# To create a new table in a PostgreSQL database, 
+# you use the following steps:
+# ---------------------------------------------------------
+# First, connect to the PostgreSQL server by 
+# calling the connect() function. 
+# The connect() function returns a connection object.
+# Second, create a cursor object by 
+# calling the cursor() method of the connection object.
+# Third, execute the CREATE TABLE by 
+# calling the execute() method of the cursor object.
+# Finally, close the connection.
+# ---------------------------------------------------------
