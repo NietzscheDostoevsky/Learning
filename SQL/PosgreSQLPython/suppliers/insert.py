5ba73294a0809acca7856c572f585f73d2e3f04f
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-04-02 13:30:10 +0530

    format documentation

--- SQL/PosgreSQLPython/suppliers/insert.py
+++ SQL/PosgreSQLPython/suppliers/insert.py
@@ -3,12 +3,13 @@
 # To insert one or more rows into a table from Python,
 # follow these steps:
 #
-# First, connect to the PostgreSQL server.
-# Next, create a cursor object from the connection object.
-# Then, execute the INSERT statement with values by calling 
-#   the execute() or executemany() method of the cursor object.
-# After that, commit the transaction by calling the commit() 
-#   method of the connection object.
-# Finally, obtain the inserted ID by calling the fetchone() method of the cursor object.
+# 1. First, connect to the PostgreSQL server.
+# 2. Next, create a cursor object from the connection object.
+# 3. Then, execute the INSERT statement with values by calling 
+#       the execute() or executemany() method of the cursor object.
+# 4. After that, commit the transaction by calling the commit() 
+#       method of the connection object.
+# 5. Finally, obtain the inserted ID by calling the fetchone() method of the cursor object.
 #
 # --------------------------------------------------------------------------------------
+
