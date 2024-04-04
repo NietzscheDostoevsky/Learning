cbc9ae0cf66887cf962e43cf46c8055dcf39603e
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-04-04 23:09:04 +0530

    not complete code, had to be updated

---
+++ SQL/Project/PythonIntegration/CurrentAfffairsDatabase/drop_tables.py
@@ -0,0 +1,20 @@
+import psycopg2
+from config import load_config
+
+
+config = load_config()
+conn = psycopg2.connect(**config) 
+cur = conn.cursor()
+
+# Get all the tables in the database
+cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+
+# Drop all the tables
+for table in cur.fetchall():
+    cur.execute("DROP TABLE IF EXISTS {}".format(table[0]))
+
+# Commit the changes
+conn.commit()
+
+# Close the connection
+conn.close()
