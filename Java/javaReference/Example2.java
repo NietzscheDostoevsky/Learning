bd0fbc903e26950cd27b12bea1cbf7bb7de3135e
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-11 12:19:10 +0530

    added code

--- Java/javaReference/Example2.java
+++ Java/javaReference/Example2.java
@@ -1,4 +1,14 @@
-public class Example2{
+class Example2{
 
-    
+    public static void main(String[] args) {
+        int num; // declaring a variable named num
+        num = 100; // assigning num a value of 100 
+
+        System.out.println("This is num: " + num) ; 
+
+        num = num * 2 ; 
+        System.out.print("The value of num * 2 is " );
+        System.out.println(num);
+    }
+
 }
