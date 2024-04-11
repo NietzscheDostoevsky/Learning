93e581069db9e927e6cb7da3d220d1dec0a9788e
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-11 13:32:17 +0530

    added conditional code

---
+++ Java/javaReference/IfSimple.java
@@ -0,0 +1,20 @@
+class IfSimple{
+
+    public static void mian(String[] args) {
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if (x < y) System.out.println("x is less tha y");
+        x = x * 2 ; 
+        if (x == y) System.out.println("x is not equal to y");
+
+        x = x * 2 ;
+        if (x > y) System.out.println("x is now grater than y");
+
+        if (x==y) System.out.println("This line should not be printted") ;
+        
+    }
+
+}
