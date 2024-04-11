f2e308bf4df7e222e2f9671b10b12c833d2fa928
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-11 13:44:50 +0530

    Update ForTest.java

--- Java/javaReference/ForTest.java
+++ Java/javaReference/ForTest.java
@@ -1,5 +1,13 @@
 class ForTest{
     public static void main(String[] args) {
-        
+
+        int x;
+        for (x=0; x<10; x = x + 1) 
+            System.out.println("This is x : " + x) ;
+
+        System.out.println("This is the update x : "  + x);
     }
 }
+
+
+// in Java, the x is updated and then the condition fails. 
