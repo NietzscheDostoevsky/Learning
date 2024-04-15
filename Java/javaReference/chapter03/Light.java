6b44b9d31e8d50524d4efea9f1f645546b981a0e
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-15 20:40:50 +0530

    added code

--- Java/javaReference/chapter03/Light.java
+++ Java/javaReference/chapter03/Light.java
@@ -1,1 +1,21 @@
 // Calculated the distance light travels using long variables
+
+class Light {
+    public static void main(String[] args) {
+
+        int lightspeed ;
+        long days ;
+        long seconds ;
+        long distance ;
+
+        // approx speed of light in mpsecond 
+        lightspeed = 186000; 
+        days = 1000; 
+        seconds = days * 24 * 60 * 60 ; 
+        distance = lightspeed * seconds ; 
+
+        System.out.print("In " + days ) ; 
+        System.out.print( " days light will travel about ") ; 
+        System.out.println(distance + " miles.") ; 
+    }
+}
