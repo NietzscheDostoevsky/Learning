35398c2da2c705a96d7521cb5486e616153d6c58
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-15 20:51:13 +0530

    Create CharDemo2.java

---
+++ Java/javaReference/chapter03/CharDemo2.java
@@ -0,0 +1,12 @@
+class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1 ;
+        //char ch2 ;
+        ch1 = 'X' ;
+        System.out.println(ch1);
+        //ch2 = ch1++ ; 
+        ch1++ ;
+        System.out.println(ch1);
+
+    }
+}
