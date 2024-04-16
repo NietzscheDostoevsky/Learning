9e6dd3808f9fb8d930c293859a4c4b3c02a165bd
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-16 16:05:25 +0530

    added code

--- Java/javaReference/chapter03/DynInit.java
+++ Java/javaReference/chapter03/DynInit.java
@@ -1,5 +1,14 @@
+
+// Demonstrating dynamic initilization 
 class DynInit {
     public static void main(String[] args) {
+        double a = 3.0 , b = 4.0 ; 
         
+        // variable c is dynamically initializad 
+
+        double c = Math.sqrt( a * a + b * b ) ; 
+
+        System.out.println("Hypotenous is " + c ) ; 
+
     }
 }
