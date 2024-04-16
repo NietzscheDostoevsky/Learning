8b0cabefe7ae50f237b5f286741abdd11a29b91b
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-16 21:29:40 +0530

    Update DynInit.java

--- Java/javaReference/chapter03/DynInit.java
+++ Java/javaReference/chapter03/DynInit.java
@@ -4,6 +4,11 @@ class DynInit {
     public static void main(String[] args) {
         double a = 3.0 , b = 4.0 ; 
         
+        /*
+         * Java allows variables to be initialized dynamically, 
+         * using any expression valid at the time the variable is declared
+         */
+        
         // variable c is dynamically initializad 
 
         double c = Math.sqrt( a * a + b * b ) ; 
