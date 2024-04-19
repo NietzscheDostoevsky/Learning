6eca12144f926806a040c03f3c574182e3403539
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-19 14:37:36 +0530

    multiple semicolon worked

---
+++ Java/QSP/TestSemicolon.java
@@ -0,0 +1,6 @@
+class TestSemicolon{
+    public static void main(String[] args) {
+        System.out.println("Testing semicolon") ; ; 
+        ; 
+    }
+}
--- Java/javaReference/chapter03/Scope.java
+++ Java/javaReference/chapter03/Scope.java
@@ -7,6 +7,11 @@ class Scope {
         if (x == 10) { // starting new scope 
             int y = 20 ; // known only to this if block 
 
+            // x and y are both known here 
+
+            System.out.println("x and y: " + x + " " + y ); 
+            x = y * 2 
+
         }
     }
 }
