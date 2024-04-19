9eed91374bf02c014cc4d786873283ccf09ae258
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-19 16:01:42 +0530

    added code

--- Java/QSP/MyInfo.java
+++ Java/QSP/MyInfo.java
@@ -1,5 +1,16 @@
 class MyInfo { 
     public static void main(String[] args) {
-        
+       
+        String name = "Saurabh" ; 
+        String nativeplace = "Bihar" ; 
+        String stream = "ECE" ; 
+        String hobbies = "Chess, F1 " ; 
+        System.out.println("Name: " + name ) ;
+        System.out.println("Stream: " + stream) ; 
+        System.out.println("Native Place: " + nativeplace) ; 
+        System.out.println("Hobbies: " + hobbies) ; 
+       
+
+
     }
 }
