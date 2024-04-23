afc099fb68f5758a8dd2382ee5719f2279cec26a
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 15:56:45 +0530

    Update ScratchPad.java

--- Java/QSP/ScratchPad.java
+++ Java/QSP/ScratchPad.java
@@ -2,8 +2,14 @@ public class ScratchPad {
     public static void main(String[] args) {
         int intValue = 300; // This value is larger than the range of a byte
         byte byteValue = (byte) intValue; // Casting int to byte
+        byte a = 40;
+        byte b = 50;
+        byte c = 100;
+        int d = a * b / c;
+        System.out.println(d + " is of type " + ((Object) d).getClass().getSimpleName());
 
-        System.out.println("Original int value: " + intValue);
-        System.out.println("Casted byte value: " + byteValue);
+        String name = "John";
+        System.out.println("Variable type of name: " + name.getClass().getSimpleName());
+
     }
 }
