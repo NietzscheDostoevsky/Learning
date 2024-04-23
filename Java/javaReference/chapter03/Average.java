7c18020a83b3b1cb6977fafa55b1b431322327fa
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 18:04:57 +0530

    Update Average.java

--- Java/javaReference/chapter03/Average.java
+++ Java/javaReference/chapter03/Average.java
@@ -4,5 +4,13 @@ class Average {
         // Calculating the averge of values
         // Learning Array delaration
 
+        double[] nums = { 10.1, 11.2, 12.3, 13.4, 14.5 };
+        double result = 0;
+        int i;
+        for (i = 0; i < 5; i++) {
+            result = result + nums[i];
+        }
+        System.out.println("Average is : " + (result / 5));
+
     }
 }
