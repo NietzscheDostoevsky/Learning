0c5174c14ac8422e1588ac461ef1e0dba63ca72d
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 14:26:11 +0530

    refirmat

--- Java/javaReference/chapter03/Scope.java
+++ Java/javaReference/chapter03/Scope.java
@@ -1,17 +1,21 @@
-class Scope { 
-    public static void main(String[] args) { 
+class Scope {
+    public static void main(String[] args) {
 
-        int x ; // known to all code withing main 
-        x = 10 ; 
+        int x; // known to all code withing main
+        x = 10;
 
-        if (x == 10) { // starting new scope 
-            int y = 20 ; // known only to this if block 
+        if (x == 10) { // starting new scope
+            int y = 20; // known only to this if block
 
-            // x and y are both known here 
+            // x and y are both known here
 
-            System.out.println("x and y: " + x + " " + y ); 
-            x = y * 2 
+            System.out.println("x and y: " + x + " " + y);
+            x = y * 2;
+        } // end if block
 
-        }
+        // y = 100; // this should give error, as y is not defines error.
+
+        // but x should still be known here .
+        System.out.println("the val of x is " + x);
     }
 }
