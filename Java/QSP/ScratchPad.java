d10f679a590c44a55ca4989e877196cee2b13bde
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 15:13:35 +0530

    Update ScratchPad.java

--- Java/QSP/ScratchPad.java
+++ Java/QSP/ScratchPad.java
@@ -1,8 +1,9 @@
 public class ScratchPad {
     public static void main(String[] args) {
-        int bar = 1;
-        {
-            int bar = 2;
-        }
+        int intValue = 300; // This value is larger than the range of a byte
+        byte byteValue = (byte) intValue; // Casting int to byte
+
+        System.out.println("Original int value: " + intValue);
+        System.out.println("Casted byte value: " + byteValue);
     }
 }
