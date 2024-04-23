21fc05ef2d3f913a6ed8903b672e7c5cd0e01d05
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 15:21:56 +0530

    error due to extensino

--- Java/javaReference/chapter03/Conversion.java
+++ Java/javaReference/chapter03/Conversion.java
@@ -1,14 +1,14 @@
 // Demostrating casting in Java 
 
-class Conversion { 
-    public static void main(String[] args) { 
-        byte b ; 
-        int i = 257 ; 
-        double d = 323.142 ; 
+class Conversion {
+    public static void main(String[] args) {
+        byte b;
+        int i = 257;
+        double d = 323.142;
 
-        System.out.println("\nConversion of int to byte " ) ; 
-        b = (byte) i ; 
-        System.out.println("i and b " + i + " " + b ) ; 
+        System.out.println("\nConversion of int to byte ");
+        b = (byte) i;
+        System.out.println("i and b " + i + " " + b);
 
         System.out.println("\nConversion of double to int.");
         i = (int) d;
@@ -18,5 +18,5 @@ class Conversion {
         b = (byte) d;
         System.out.println("d and b " + d + " " + b);
 
-    } 
-} 
+    }
+}
