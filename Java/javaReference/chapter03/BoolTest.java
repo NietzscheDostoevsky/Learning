2444c62c2d3ac900752b1c61ba8e7c1d24cd4050
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 13:12:19 +0530

    Create BoolTest.java

---
+++ Java/javaReference/chapter03/BoolTest.java
@@ -0,0 +1,13 @@
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+
+        b = false;
+        System.out.println(b);
+        b = true;
+        System.out.println(b);
+
+        // A bool val can control the if statement
+
+    }
+}
