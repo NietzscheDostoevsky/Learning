fde2964a4e2ca109be564411f21cab2222eca615
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 14:29:16 +0530

    lifecucle of a variable

--- Java/javaReference/chapter03/Lifetime.java
+++ Java/javaReference/chapter03/Lifetime.java
@@ -1,8 +1,17 @@
 class Lifetime {
-    public static void main(String[] args) { 
-        // Demostrating lifetime of a variable using for loop 
+    public static void main(String[] args) {
+        // Demostrating lifetime of a variable using for loop
 
-        int x ; 
+        int x;
 
-        for ( x = 0; x < 3 ; x ++ ) { 
-            int y = -1 ; // This y will be iniatilized each time this block is entered, ie for each for interation
+        for (x = 0; x < 3; x++) {
+            int y = -1; // This y will be iniatilized each time this block is entered, ie for each for
+                        // interation
+            System.out.println("y is : " + y);
+            y = 100;
+            System.out.println("y is now : " + y);
+
+            // Y will alwyas reilitialize to -1 each time for loop runs
+        }
+    }
+}
