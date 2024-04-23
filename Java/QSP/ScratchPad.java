1155bd7b57536127f525a328a085f678816744b3
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 17:57:52 +0530

    array

--- Java/QSP/ScratchPad.java
+++ Java/QSP/ScratchPad.java
@@ -1,15 +1,8 @@
 public class ScratchPad {
     public static void main(String[] args) {
-        int intValue = 300; // This value is larger than the range of a byte
-        byte byteValue = (byte) intValue; // Casting int to byte
-        byte a = 40;
-        byte b = 50;
-        byte c = 100;
-        int d = a * b / c;
-        System.out.println(d + " is of type " + ((Object) d).getClass().getSimpleName());
-
-        String name = "John";
-        System.out.println("Variable type of name: " + name.getClass().getSimpleName());
+        int[] month_days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31,
+                30, 31 };
+        System.out.println("May has " + month_days[4] + " days.");
 
     }
 }
