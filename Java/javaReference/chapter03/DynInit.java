865b7d7fd87a9a3bc59aa1b385fae32d7e87461c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 14:11:06 +0530

    dynamnic initialization

--- Java/javaReference/chapter03/DynInit.java
+++ Java/javaReference/chapter03/DynInit.java
@@ -1,19 +1,22 @@
 
 // Demonstrating dynamic initilization 
+
+/* The key point here is that the initialization expression may use any element valid at the time of the initialization,
+including calls to methods, other variables, or literals. */
 class DynInit {
     public static void main(String[] args) {
-        double a = 3.0 , b = 4.0 ; 
-        
+        double a = 3.0, b = 4.0;
+
         /*
-         * Java allows variables to be initialized dynamically, 
+         * Java allows variables to be initialized dynamically,
          * using any expression valid at the time the variable is declared
          */
-        
-        // variable c is dynamically initializad 
 
-        double c = Math.sqrt( a * a + b * b ) ; 
+        // variable c is dynamically initializad
 
-        System.out.println("Hypotenous is " + c ) ; 
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println("Hypotenous is " + c);
 
     }
 }
