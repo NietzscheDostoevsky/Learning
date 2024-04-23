a51b71a2ff8cb5a293d4a88842c8d7917b46ebe2
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 12:23:39 +0530

    added documentation

--- Java/javaReference/chapter03/CharDemo.java
+++ Java/javaReference/chapter03/CharDemo.java
@@ -1,11 +1,15 @@
 class CharDemo {
     public static void main(String[] args) {
-        char ch1, ch2 ;
+        char ch1, ch2;
         ch1 = 88; // code for X (UNIODE)
         ch2 = 'Y';
 
-        System.out.print("ch1 and ch2 : " );
-        System.out.println(ch1 + " " + ch2) ; 
+        System.out.print("ch1 and ch2 : ");
+        System.out.println(ch1 + " " + ch2);
 
     }
 }
+
+// java uses UNICODE by default,
+// but the first 127 values on Unicode are same as
+// ASCII character set.
