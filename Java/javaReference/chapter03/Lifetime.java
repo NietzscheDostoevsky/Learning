f99bcc515ccb73e51885df83013873bfa216e725
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 14:26:04 +0530

    Create Lifetime.java

---
+++ Java/javaReference/chapter03/Lifetime.java
@@ -0,0 +1,8 @@
+class Lifetime {
+    public static void main(String[] args) { 
+        // Demostrating lifetime of a variable using for loop 
+
+        int x ; 
+
+        for ( x = 0; x < 3 ; x ++ ) { 
+            int y = -1 ; // This y will be iniatilized each time this block is entered, ie for each for interation
