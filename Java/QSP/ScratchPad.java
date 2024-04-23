e33413b2bbebefc892084be36044fe702ad6047e
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-23 12:35:52 +0530

    worked

--- Java/QSP/ScratchPad.java
+++ Java/QSP/ScratchPad.java
@@ -1,5 +1,9 @@
 public class ScratchPad {
     public static void main(String[] args) {
         int a = 'X';
+        System.out.println(a);
+
+        char b = 33;
+        System.out.println(b);
     }
 }
