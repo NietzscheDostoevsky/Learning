a72016784118435e0591ea12ef33cc5a0e022c84
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-25 19:10:18 +0530

    Create Vehicle.java

---
+++ Java/QSP/Vehicle.java
@@ -0,0 +1,17 @@
+class Vehicle {
+
+    public static void main(String[] args) {
+        byte wheels = 4;
+        byte doors = 5;
+        byte seats = 5;
+        byte fuelTankCapacity = 50; // in liters
+        byte gears = 5;
+
+        System.out.println("Number of wheels = " + wheels);
+        System.out.println("Number of doors = " + doors);
+        System.out.println("Number of seats = " + seats);
+        System.out.println("Fuel tank capacity = " + fuelTankCapacity + " liters");
+        System.out.println("Number of gears = " + gears);
+    }
+
+}
