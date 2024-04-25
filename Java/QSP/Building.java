76dddc08eb19bd9827bd113d712b690ec0ae8547
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-25 19:09:12 +0530

    Update Building.java

--- Java/QSP/Building.java
+++ Java/QSP/Building.java
@@ -1,3 +1,17 @@
-public class Building {
-    
+class Building {
+
+    public static void main(String[] args) {
+        byte floors = 10;
+        byte roomsPerFloor = 20;
+        byte elevators = 3;
+        byte windowsPerRoom = 2;
+        byte exitsPerRoom = 1;
+
+        System.out.println("Number of floors = " + floors);
+        System.out.println("Rooms per floor = " + roomsPerFloor);
+        System.out.println("Number of elevators = " + elevators);
+        System.out.println("Windows per room = " + windowsPerRoom);
+        System.out.println("Exits per room : " + exitsPerRoom);
+    }
+
 }
