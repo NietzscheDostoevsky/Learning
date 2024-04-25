4b6bf28177d881d64a4bc2001590041f3358c966
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-25 19:22:02 +0530

    Update Hospital.java

--- Java/QSP/DataTypes/Hospital.java
+++ Java/QSP/DataTypes/Hospital.java
@@ -1,3 +1,16 @@
-public class Hospital {
-    
+class Hospital {
+
+    public static void main(String[] args) {
+        short numberOfBeds = 1500;
+        short numberOfDoctors = 250;
+        short numberOfNurses = 500;
+        short numberOfOperatingRooms = 20;
+        short numberOfAmbulances = 20;
+
+        System.out.println("Number of beds: " + numberOfBeds);
+        System.out.println("Number of doctors: " + numberOfDoctors);
+        System.out.println("Number of nurses: " + numberOfNurses);
+        System.out.println("Number of operating rooms: " + numberOfOperatingRooms);
+        System.out.println("Number of ambulances: " + numberOfAmbulances);
+    }
 }
