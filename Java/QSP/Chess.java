517bf21589b73deafe17bd38b9be69ce473247a0
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-25 19:04:23 +0530

    added code

---
+++ Java/QSP/Chess.java
@@ -0,0 +1,17 @@
+class Chess {
+
+    public static void main(String[] args) {
+        byte players = 2;
+        byte colors = 2;
+        byte pieces = 32;
+        byte moves = 60;
+        byte possibleResults = 3;
+
+        System.out.println("Chess Players = " + players);
+        System.out.println("Chess piece colors = " + colors);
+        System.out.println("Chess total pieces" + pieces);
+        System.out.println("Chess average moves = " + moves);
+        System.out.println("Chess possible results : " + possibleResults);
+    }
+
+}
