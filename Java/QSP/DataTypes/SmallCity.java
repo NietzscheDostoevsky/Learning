07d72b938b12d8d81b3383223f078cce7b1ebc31
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-25 19:17:31 +0530

    Update SmallCity.java

---
+++ Java/QSP/DataTypes/SmallCity.java
@@ -0,0 +1,16 @@
+class SmallCity {
+
+    public static void main(String[] args) {
+        short population = 10000;
+        short areaInSquareKm = 200;
+        short numberOfBuildings = 500;
+        short numberOfParks = 20;
+        short numberOfSchools = 10;
+
+        System.out.println("Population: " + population);
+        System.out.println("Area in square kilometers: " + areaInSquareKm);
+        System.out.println("Number of buildings: " + numberOfBuildings);
+        System.out.println("Number of parks: " + numberOfParks);
+        System.out.println("Number of schools: " + numberOfSchools);
+    }
+}
