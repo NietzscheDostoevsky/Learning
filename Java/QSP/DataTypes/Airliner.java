f445209236138117e881e3b89dcf373b45f7b4e1
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-25 19:17:42 +0530

    Update Airliner.java

--- Java/QSP/DataTypes/Airliner.java
+++ Java/QSP/DataTypes/Airliner.java
@@ -1,3 +1,16 @@
-public class Airliner {
-    
+class Airliner {
+
+    public static void main(String[] args) {
+        short numberOfEngines = 4;
+        short seatingCapacity = 300;
+        short maximumAltitudeInFeet = 40000;
+        short maximumSpeedInKmph = 900;
+        short numberOfCrewMembers = 15;
+
+        System.out.println("Number of engines: " + numberOfEngines);
+        System.out.println("Seating capacity: " + seatingCapacity);
+        System.out.println("Maximum altitude (feet): " + maximumAltitudeInFeet);
+        System.out.println("Maximum speed (kmph): " + maximumSpeedInKmph);
+        System.out.println("Number of crew members: " + numberOfCrewMembers);
+    }
 }
