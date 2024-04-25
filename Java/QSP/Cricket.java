3d384a393cb0822f1072c33d3c0a7befa5834aeb
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-25 19:06:28 +0530

    Update Cricket.java

---
+++ Java/QSP/Cricket.java
@@ -0,0 +1,17 @@
+class Cricket {
+
+    public static void main(String[] args) {
+        byte teams = 12;
+        byte playersPerTeam = 11;
+        byte umpires = 3;
+        byte oversPerInning = 50;
+        byte possibleResults = 3;
+
+        System.out.println("Cricket Teams = " + teams);
+        System.out.println("Players per team = " + playersPerTeam);
+        System.out.println("Total umpires = " + umpires);
+        System.out.println("Overs per inning = " + oversPerInning);
+        System.out.println("Possible results : " + possibleResults);
+    }
+
+}
