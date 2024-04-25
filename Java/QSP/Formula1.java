0a2d5b2325ec5c97545a18dbf511585ad302655e
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-25 19:04:49 +0530

    Create Formula1.java

---
+++ Java/QSP/Formula1.java
@@ -0,0 +1,17 @@
+class Formula1 {
+
+    public static void main(String[] args) {
+        byte teams = 10;
+        byte driversPerTeam = 2;
+        byte circuits = 22;
+        byte lapsPerRace = 60;
+        byte possibleResults = 3;
+
+        System.out.println("Formula 1 Teams = " + teams);
+        System.out.println("Drivers per team = " + driversPerTeam);
+        System.out.println("Total circuits = " + circuits);
+        System.out.println("Average laps per race = " + lapsPerRace);
+        System.out.println("Possible results : " + possibleResults);
+    }
+
+}
