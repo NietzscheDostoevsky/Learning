8bd5767303c43ec062ae9124dec140f6ef198ae2
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-26 14:51:27 +0530

    clarified variable

--- Java/QSP/DataTypes/Planet.java
+++ Java/QSP/DataTypes/Planet.java
@@ -1,16 +1,16 @@
 class Planet {
 
     public static void main(String[] args) {
-        int diameterInKilometers = 12742;
+        int diameter = 12742;
         int distanceFromSun = 149_600_000;
         int mass = 1_972_000_000;
         int surfaceTemperature = 15;
         int numberOfMoons = 23;
 
         System.out.println("*****DETAILS OF NEW FOUND PLANET********");
-        System.out.println("Diameter (km): " + diameterInKilometers);
+        System.out.println("Diameter (km): " + diameter);
         System.out.println("Distance from Sun (million km): " + distanceFromSun);
-        System.out.println("Mass (kg): " + mass);
+        System.out.println("Mass (in Thousand kg): " + mass);
         System.out.println("Surface temperature (°C): " + surfaceTemperature);
         System.out.println("Number of moons: " + numberOfMoons);
     }
