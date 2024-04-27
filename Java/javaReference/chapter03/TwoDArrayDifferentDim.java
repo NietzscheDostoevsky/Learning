ed4831824584b30f159ba4e4b120d8a7a1328d20
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-27 14:02:59 +0530

    Update TwoDArrayDifferentDim.java

---
+++ Java/javaReference/chapter03/TwoDArrayDifferentDim.java
@@ -0,0 +1,7 @@
+// Demonstrating differing size of rows after each row 
+
+class TwoDArrayDifferentDim {
+    public static void main(String[] args) {
+        int [][] twoD = new int[4][]; 
+    }
+}
