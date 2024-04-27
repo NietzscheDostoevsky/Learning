d37c80868f28d7f074f71f351c06f1d56ee95edc
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-27 16:08:48 +0530

    float dtype

---
+++ Java/QSP/DataTypes/MilkProduction.java
@@ -0,0 +1,16 @@
+public class MilkProduction {
+    public static void main(String[] args) {
+        float milk = 100000.1f ; 
+        float ghee = 171717.3f; 
+        float paneer = 13434.5f; 
+        float butter = 500.4f ; 
+        float dahi = 3000.2f ; 
+
+        System.out.println("Daily milk production : " + milk + " litres");
+        System.out.println("Daily ghee production : " + ghee + " litres");
+        System.out.println("Daily panner production : " + paneer + " kg");
+        System.out.println("Daily ghee production : " + butter + " litres");
+        System.out.println("Daily milk production : " + dahi + " kg");
+        
+    }
+}
