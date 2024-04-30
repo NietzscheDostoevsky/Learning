e7528574237d9a83cb597949d7fceb15af1b9193
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-30 15:16:24 +0530

    Create AreaCircle.java

---
+++ Java/QSP/AreaCircle.java
@@ -0,0 +1,15 @@
+class AreaCircle {
+
+    public static void main(String[] args) {
+        
+        final float pi = 3.1419f ; 
+        int radius = 5 ; 
+
+        double perimeter ; 
+        perimeter = 2 * pi * radius ; 
+
+        System.out.println("Perimeter = " + perimeter);
+
+    }
+    
+}
