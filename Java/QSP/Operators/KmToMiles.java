b45331fe475e17d88d175838848476312a57ba2b
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-30 20:29:01 +0530

    done

--- Java/QSP/Operators/KmToMiles.java
+++ Java/QSP/Operators/KmToMiles.java
@@ -1,3 +1,18 @@
-public class KmToMiles {
+class KmToMiles {
 
+    public static void main(String[] args) {
+        double distanceKm = 14.0; // in km
+        double timeInMinutes = 45.5f;
+
+        // Conversion factor
+        double milesPerKm = 1 / 1.6; // miles per km
+
+        double distanceMile = distanceKm * milesPerKm;
+        double speedMPH = distanceMile / (timeInMinutes / 60.0);
+
+        // displaying the output
+
+        System.out.println("Speed in MPH : " + speedMPH + "MPH");
+
+    }
 }
