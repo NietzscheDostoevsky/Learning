36b4e04a10fa68c8be37a5a9397841e88d77d3e9
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-30 15:35:00 +0530

    rectangles done

--- Java/QSP/Operators/Rectangle.java
+++ Java/QSP/Operators/Rectangle.java
@@ -3,6 +3,9 @@ class Rectangle {
         float width = 4.5f ; 
         float height = 7.9f ; 
         float area ; 
-        
+
+        area = width * height ; 
+
+        System.out.println("Area of the rectangle = " + area);
     }
 }
