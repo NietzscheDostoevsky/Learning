8f8465d8661ad1a116934d9eb91652f7e5014ad5
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-30 15:19:03 +0530

    added area

--- Java/QSP/AreaCircle.java
+++ Java/QSP/AreaCircle.java
@@ -6,9 +6,12 @@ class AreaCircle {
         int radius = 5 ; 
 
         double perimeter ; 
+        double area ; 
         perimeter = 2 * pi * radius ; 
+        area = pi * radius * radius; 
 
         System.out.println("Perimeter = " + perimeter);
+        System.out.println("Area = " + area);
 
     }
     
