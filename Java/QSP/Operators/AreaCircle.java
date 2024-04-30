bc2036a7e9216e33f2eeba528c41125eae09a03f
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-30 20:30:05 +0530

    done

--- Java/QSP/Operators/AreaCircle.java
+++ Java/QSP/Operators/AreaCircle.java
@@ -1,18 +1,18 @@
 class AreaCircle {
 
     public static void main(String[] args) {
-        
-        final float pi = 3.147f ; 
-        float radius = 5.5f ; 
 
-        double perimeter ; 
-        double area ; 
-        perimeter = 2 * pi * radius ; 
-        area = pi * radius * radius; 
+        final float pi = 3.147f;
+        float radius = 5.5f;
+
+        double perimeter;
+        double area;
+        perimeter = 2 * pi * radius;
+        area = pi * radius * radius;
 
         System.out.println("Perimeter = " + perimeter);
         System.out.println("Area = " + area);
 
     }
-    
+
 }
