5747368e22e9ed393b873c9d3a4dff154e90d9d8
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-04-30 18:38:40 +0530

    Update PopulationProjection.java

--- Java/QSP/Operators/PopulationProjection.java
+++ Java/QSP/Operators/PopulationProjection.java
@@ -1,3 +1,15 @@
 public class PopulationProjection {
 
+    public static void main(String[] args) {
+
+        // Given Data
+        final int currentPopulation = 312_032_486; // people
+        final int year = 365; // days
+        final int birth = 7;
+        final int death = 13;
+        final int immigration = 45;
+
+        // Calculated Data
+
+    }
 }
