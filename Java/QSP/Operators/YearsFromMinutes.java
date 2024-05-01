156f0126790ef0cccd0b60924ec9a8715e14f141
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-01 21:16:32 +0530

    success

--- Java/QSP/Operators/YearsFromMinutes.java
+++ Java/QSP/Operators/YearsFromMinutes.java
@@ -4,5 +4,17 @@ public class YearsFromMinutes {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        final int year = 365;
+        System.out.print("Enter the number of minutes : ");
+        long minutes = sc.nextLong();
+
+        // Calculations
+        long totalDays = minutes / (24 * 60);
+        long totalYears = totalDays / year;
+        long remainingDays = totalDays % year;
+
+        System.out.println(
+                minutes + " minutes " + "is approximately " + totalYears + " years and " + remainingDays + " days ");
     }
 }
