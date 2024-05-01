bdb3dfba34f7cf7c9708a045b98314ec82a6b530
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-01 14:59:06 +0530

    formated

--- Java/QSP/Operators/Addition.java
+++ Java/QSP/Operators/Addition.java
@@ -3,9 +3,9 @@ class Addition {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ; 
-        System.out.print("Enter num 1") ; 
+        System.out.print("Enter num 1 : ") ; 
         int num1 = sc.nextInt() ; 
-        System.out.print("Enter num 2") ; 
+        System.out.print("Enter num 2 : ") ; 
         int num2 = sc.nextInt() ; 
 
         int sum = num1 + num2 ; 
