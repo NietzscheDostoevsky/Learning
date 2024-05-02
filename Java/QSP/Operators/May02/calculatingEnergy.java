38ec7df451dd1c7acbed53a018c5d9d1c38cf6cd
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-02 22:10:38 +0530

    success

--- Java/QSP/Operators/May02/calculatingEnergy.java
+++ Java/QSP/Operators/May02/calculatingEnergy.java
@@ -1,5 +1,21 @@
-package May02;
+
+import java.util.Scanner;
 
 public class calculatingEnergy {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the amount of water in kilograms : ");
+        double m = sc.nextDouble();
+        System.out.print("Enter the initial terperatue: ");
+        double initialTemp = sc.nextDouble();
+        System.out.print("Enter the final temperature: ");
+        double finalTemp = sc.nextDouble();
+
+        // Calculating energy
+        double energy;
+        energy = m * (finalTemp - initialTemp) * 4184;
+        System.out.println("The energy needed is " + energy + " joules");
+    }
+
 }
