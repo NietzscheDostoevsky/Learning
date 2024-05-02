c64e934f51084b2cdf6233f1bf41c13a8eddcec3
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-02 22:43:45 +0530

    compound ing

--- Java/QSP/Operators/May02/compoundValue.java
+++ Java/QSP/Operators/May02/compoundValue.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class compoundValue {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the monthly saving amount : ");
+        double principal = sc.nextDouble();
+
+        // Calculations
+        double interestRate = 0.05 / 12;
+        int t = 6;
+        double amount = principal * Math.pow((1 + interestRate), t);
+
+        System.out.println("After six months, the account value is Rs " + amount);
+
     }
-    
+
 }
