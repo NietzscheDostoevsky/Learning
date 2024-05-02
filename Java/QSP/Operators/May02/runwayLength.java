1cc3b0a01f0d404477a8816f42f736f0af820b97
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-02 22:19:24 +0530

    success

--- Java/QSP/Operators/May02/runwayLength.java
+++ Java/QSP/Operators/May02/runwayLength.java
@@ -4,6 +4,16 @@ public class runwayLength {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter speed and accelaration : ");
+        double speed = sc.nextDouble();
+        double accelaration = sc.nextDouble();
+
+        // Calculation
+
+        double length = (speed * speed) / (2 * accelaration);
+        System.out.println("The min runway length for this airplane is " + length);
+
     }
-    
+
 }
