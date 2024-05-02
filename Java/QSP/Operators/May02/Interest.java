c66f503202dfa73cbb435fe62ef0b4e672a93adc
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-02 23:12:44 +0530

    success

--- Java/QSP/Operators/May02/Interest.java
+++ Java/QSP/Operators/May02/Interest.java
@@ -4,6 +4,14 @@ public class Interest {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("Enter interest rate: ");
+        double interestRate = sc.nextDouble();
+
+        // Calculations
+        double interest = balance * (interestRate / 1200);
+        System.out.println("The interest is " + interest);
     }
-    
+
 }
