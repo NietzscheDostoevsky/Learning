67276d6af849273281fd765a45d423d231c62521
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-04 15:22:29 +0530

    reversed using loops

---
+++ Java/QSP/Operators/Apr30/May04/ReverseNumberLoop.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+
+public class ReverseNumberLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a 3 digit number : ");
+
+        int num = sc.nextInt();
+        int dup = num ; 
+        int rev = 0 ; 
+        int rem  = 0 ; 
+
+        int i ; 
+        for(i=0;i<3;i++){
+
+            rem = num % 10 ; 
+            rev = (rev*10) + rem ; 
+            num = num / 10 ; 
+        }
+        System.out.println(dup + " Number's reverse value is  " + rev);
+    }
+    
+}
