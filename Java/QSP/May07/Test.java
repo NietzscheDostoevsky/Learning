91bca75fc7f3ce9a99f7f18db5fbd8e0cacda9b2
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-07 15:20:53 +0530

    Create Test.java

---
+++ Java/QSP/May07/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        
+       a = a - (a - b) ; 
+       b = b 
+       a = a - b ; 
+
+       System.out.println(a);
+       System.out.println(b);
+
+    }
+}
