452abcba4212ceb88377b6486a061690204923e0
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-07 14:53:57 +0530

    Create NumberSwap.java

---
+++ Java/QSP/May07/NumberSwap.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+
+class NumberSwap {
+    public static void main(String[] args) {
+
+        int num1 = 5 ;
+        int num2 = 6 ;
+
+       // num1, num2 = num2, num1 ; 
+
+        System.out.println(num1);
+        System.out.println(num2);
+        
+    }
+}
