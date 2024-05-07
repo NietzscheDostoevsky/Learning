1fffb751aa0d41d489510f509b2f0226961fb02c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-07 11:48:42 +0530

    Create TechNumber.java

---
+++ Java/QSP/Operators/May02/TechNumber.java
@@ -0,0 +1,16 @@
+package May02;
+
+public class TechNumber {
+    
+    public static void main(String[] args) {
+        int number = 2025 ; 
+        int num1 = number / 100 ; 
+        int num2 = number % 2 ; 
+
+        boolean val ; 
+
+        //Calculation 
+
+        
+    }
+}
