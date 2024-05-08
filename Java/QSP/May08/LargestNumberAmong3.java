76dced5dc19dc6245a933802a0eb3c02b9c6db3a
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-08 21:50:10 +0530

    success

--- Java/QSP/May08/LargestNumberAmong3.java
+++ Java/QSP/May08/LargestNumberAmong3.java
@@ -1,8 +1,23 @@
-import java.util.Scanner ; 
+import java.util.Scanner;
 
 class LargestNumberAmong3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num1 : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter num2 : ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter num3 : ");
+        int num3 = sc.nextInt();
+
+        boolean logic1 = num1 > num2;
+        boolean logic2 = num1 > num3;
+        boolean logic3 = num2 > num3;
+
+        int largest = logic1 ? (logic2 ? num1 : num3) : (logic3 ? num2 : num3);
+        String str = String.format("The largest value among %d, %d, %d is : %d", num1, num2, num3, largest);
+        System.out.println(str);
     }
 
 }
