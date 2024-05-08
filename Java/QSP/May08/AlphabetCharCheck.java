c5f9b8175551f06f64ff9472b1d2f363e2705ef7
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-08 22:10:18 +0530

    success

--- Java/QSP/May08/AlphabetCharCheck.java
+++ Java/QSP/May08/AlphabetCharCheck.java
@@ -4,6 +4,14 @@ public class AlphabetCharCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the alphabet : ");
+        char ch = sc.next().toLowerCase().charAt(0);
+
+        boolean logic = (ch > 96 && ch < 123);
+        String str1 = logic ? " an Alphabet" : "not an alphabet";
+        String str2 = String.format("The entered character %c is %s", ch, str1);
+        System.out.println(str2);
+
     }
 
 }
