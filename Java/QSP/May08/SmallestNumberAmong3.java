020b37ab05e8fd799ce3d139f4ca0fb009572e8c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-08 21:56:12 +0530

    success

--- Java/QSP/May08/SmallestNumberAmong3.java
+++ Java/QSP/May08/SmallestNumberAmong3.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
 
-public class SmallestNumberAmong3 {
+class SmallestNumberAmong3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter num1 : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter num2 : ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter num3 : ");
+        int num3 = sc.nextInt();
+
+        boolean logic1 = num1 < num2;
+        boolean logic2 = num1 < num3;
+        boolean logic3 = num2 < num3;
+
+        int smallest = logic1 ? (logic2 ? num1 : num3) : (logic3 ? num2 : num3);
+        String str = String.format("The smalles value among %d, %d, %d is : %d", num1, num2, num3, smallest);
+        System.out.println(str);
     }
 
 }
