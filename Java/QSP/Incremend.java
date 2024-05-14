a0c0258011a4518a9362eb041df0e6c167e4318a
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-14 07:52:24 +0530

    java code

---
+++ Java/QSP/Incremend.java
@@ -0,0 +1,9 @@
+class Incremend{
+    public static void main(String[] args) {
+        
+        int a = 5 ; 
+        int b = a++ ; 
+        int c = --b ; 
+        System.out.println(c);
+    }
+}
---
+++ Java/QSP/Test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+class Test{
+	public static void main(String[] args){
+	
+	Scanner sc = new Scanner(system.in); 
+	System.out.print("Enter num1 : ") ; 
+	int num1 = sc.getInt() ; 
+	System.out.print("Enter Num1 : ") ; 
+	int num2 = sc.getInt() ; 
+	System.out.print("Enter num3 : ") ; 
+	int num3 = sc.getInt() ; 
+
+	// logic 
+	boolean logic1 = num1 > num2 ; 
+	boolean logic2 = num1 > num3 ; 
+	boolean logic3 = num2 > num3 ; 
+
+	int largest = logic1 ? logic2 
+	}
+}
