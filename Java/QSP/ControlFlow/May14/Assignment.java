9c0c5dee37a91c1c84e607f56fd9edb7b8ccedae
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-14 14:42:31 +0530

    Create Assignment.java

---
+++ Java/QSP/ControlFlow/May14/Assignment.java
@@ -0,0 +1,17 @@
+import java.util.Scanner ; 
+class Assignment{
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in) ;
+		int x = 0 // delcaration
+		System.out.print("Enter a number "); 
+		int y = sc.nextInt(); 
+
+		if (y > 0 ) {
+			x = 1 ; 
+		}
+		System.out.println("x = " + x);
+
+		if 
+	}
+}
