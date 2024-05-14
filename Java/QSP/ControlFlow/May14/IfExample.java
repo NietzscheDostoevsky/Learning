e437e72f7777605d2a79a4baf247066d62a4c592
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-14 13:02:56 +0530

    Create IfExample.java

---
+++ Java/QSP/ControlFlow/May14/IfExample.java
@@ -0,0 +1,13 @@
+class IfExample{
+	public static void main(String[] args) {
+		System.out.println("Execution Starts ");
+
+		if(false) {
+			System.out.println("Hello ");
+			System.out.println("Byeeees");
+
+		}
+		System.out.println("Execution Ends ");
+
+	}
+}
