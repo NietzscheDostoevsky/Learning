c85ad903521e24659bd13d3bdf5c016969023475
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-14 13:13:35 +0530

    method 2

---
+++ Java/QSP/ControlFlow/May14/EvenOddWithoutMod2.java
@@ -0,0 +1,12 @@
+class EvenOddWithoutMod2{
+	public static void main(String[] args) {
+
+		int num1 = 22 ; 
+		
+		if (num1/2 == num1/2.0 )
+			System.out.println("Number is Even");
+		if (num1/2 != num1 / 2.0)
+			System.out.println("Number is Odd");
+
+	}
+}
