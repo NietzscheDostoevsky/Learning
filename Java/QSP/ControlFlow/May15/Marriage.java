b4feb2027964da171634906c90fffc85b4aee35a
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-15 12:54:57 +0530

    Update Marriage.java

--- Java/QSP/ControlFlow/May15/Marriage.java
+++ Java/QSP/ControlFlow/May15/Marriage.java
@@ -1,13 +1,23 @@
+import java.util.Scanner ; 
+
 class Marriage{
 	public static void main(String[] args) {
-		int age = 22 ;
-		int money = 2_000_000_000 ; 
-		String surname = "Adani" ; 
+		Scanner sc = new Scanner(System.in) ; 
+		System.out.print("Enter Age: ");
+		int age = sc.nextInt() ;
+		System.out.print("Enter Money : ");
+		long money = sc.nextLong();
+		System.out.print("Enter Surname: "); 
+		String surname = sc.next().strip().toLowerCase() ; 
 
-		if((age > 21) && (money > 1_00_00_000) && (surname == "Ambani") ){
-			System.out.println("Elegible") ;
-		}else{
-			System.out.println("Not Eligible");
+		if(age > 21) {
+			if((money > 1_00_00_000) || (surname.equals("ambani") ) ){ // use surname.equals() method for string comparison
+				System.out.print("Eligible");
 		}
+		}else{
+			System.out.print("Not eligible");
+		}
+		
 	}
 }
+
