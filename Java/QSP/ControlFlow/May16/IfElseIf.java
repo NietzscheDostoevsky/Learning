83ec9b6b94b0002dd9e2bec6f1c403bd6bbfda2d
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-16 14:04:49 +0530

    Create IfElseIf.java

---
+++ Java/QSP/ControlFlow/May16/IfElseIf.java
@@ -0,0 +1,17 @@
+class IfElseIf{
+	public static void main(String[] args) {
+		if(false){
+			System.out.println("Hello from if block 1");
+		} else 
+		if(false) {
+			System.out.println("Hello from if block 2");
+		
+		} else 
+		if(false) {
+			System.out.println("Hello from if block 3");
+
+		} else  {
+			System.out.println("Hello from if block 4");
+		}
+	}
+}
