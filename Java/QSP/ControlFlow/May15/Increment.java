0776871a68a54ea3fda29867780400ad3665baa1
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-16 14:05:42 +0530

    Create Increment.java

---
+++ Java/QSP/ControlFlow/May15/Increment.java
@@ -0,0 +1,12 @@
+public class Increment {
+    public static void main(String[] args) {
+        int a = 3 ; 
+        int b = a++ ; 
+    
+
+        // System.out.println(a);
+        System.out.print(b);
+
+    }
+    
+}
