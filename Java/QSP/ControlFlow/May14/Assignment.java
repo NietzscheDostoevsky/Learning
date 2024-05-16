58d88130b6cb93c5a5ddaee7c0202a0f88fca005
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-16 08:49:37 +0530

    removed unused

--- Java/QSP/ControlFlow/May14/Assignment.java
+++ Java/QSP/ControlFlow/May14/Assignment.java
@@ -1,17 +1,17 @@
-import java.util.Scanner ; 
-class Assignment{
-	public static void main(String[] args){
+import java.util.Scanner;
 
-		Scanner sc = new Scanner(System.in) ;
-		int x = 0 // delcaration
-		System.out.print("Enter a number "); 
-		int y = sc.nextInt(); 
+class Assignment {
+	public static void main(String[] args) {
 
-		if (y > 0 ) {
-			x = 1 ; 
+		Scanner sc = new Scanner(System.in);
+		int x = 0; // delcaration
+		System.out.print("Enter a number ");
+		int y = sc.nextInt();
+
+		if (y > 0) {
+			x = 1;
 		}
 		System.out.println("x = " + x);
 
-		if 
 	}
 }
