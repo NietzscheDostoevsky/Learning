05a7946475f25b5f252d98f1ee3d5af9bfca5062
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-20 14:03:37 +0530

    Update SwitchExamples.java

--- Java/QSP/ControlFlow/May18/SwitchExamples.java
+++ Java/QSP/ControlFlow/May18/SwitchExamples.java
@@ -3,17 +3,18 @@ import java.util.Scanner ;
 class SwitchExample{
 	public static void main(String[] args) {
 		
-		int a = -97; 
+		int a = 3; 
 		switch(a) {
 
+		default: System.out.println("ddefssult1 "); 
 		case 1 : System.out.println("CASE 1 " + a);// a++; 
 		case 3 : System.out.println("CASE 3 " + a); a++ ; 
 		case 7 : System.out.println("CASE 7 " + a); a++ ;//break ; 
 		case 2 : System.out.println("CASE 2 " + a); //break ; 
-		case 66 : System.out.println("CASE 66 "); break ;
-		case -'b' : System.out.println("CASE a "); break ;
-		case 89 : System.out.println("CASE 1 ");   break ;  
-		default: System.out.println("ddefssult1 "); 	
+		case 66 : System.out.println("CASE 66 "); //break ;
+		case -'b' : System.out.println("CASE a ");// break ;
+		case 89 : System.out.println("CASE 1 ");   //break ;  
+			
 		}
 	}
 }
