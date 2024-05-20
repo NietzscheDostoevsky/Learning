b29ea5feb41d43d93f11a75a1b59a8eeedddde1b
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-20 14:03:51 +0530

    Create MultipleFor.java

---
+++ Java/QSP/ControlFlow/May20/MultipleFor.java
@@ -0,0 +1,9 @@
+class MultipleFor{
+
+    public static void main(String[] args) {
+        for (int i = 0, j = 10; i < j; i++, j--) {
+            System.out.println("i = " + i + ", j = " + j);
+        }
+    }
+
+}
