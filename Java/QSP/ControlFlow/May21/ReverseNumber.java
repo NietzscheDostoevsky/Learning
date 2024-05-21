ad73792d95133ae9e343a8bc079b661a27551a44
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-21 11:45:34 +0530

    Create ReverseNumber.java

---
+++ Java/QSP/ControlFlow/May21/ReverseNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner ; 
+
+class ReverseNumber{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in) ; 
+		System.out.print("Enter the number to reverse: ");
+		int num = sc.nextInt() ; 
+		int rev = 0 ; 
+		for(int i = num ; i!=0 ; i = i/10){
+			rev = rev*10 + (i%10) ;
+
+		}
+		System.out.println(num + " Number's reverse value is  " + rev);
+
+	}
+}
