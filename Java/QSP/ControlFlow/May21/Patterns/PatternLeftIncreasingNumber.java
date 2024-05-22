cc02e47f301c908ed80798c8e11ed71cb86fbb8c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-22 14:30:50 +0530

    added logic comments

--- Java/QSP/ControlFlow/May21/Patterns/PatternLeftIncreasingNumber.java
+++ Java/QSP/ControlFlow/May21/Patterns/PatternLeftIncreasingNumber.java
@@ -6,20 +6,22 @@ public class PatternLeftIncreasingNumber {
         System.out.print("Enter the number of rows to print: ");
         int input = sc.nextInt() ; 
         int n = input;
-        int num = 0;
+        int num = 1;
 
 
         for (int i = 0; i < n; i++) {
 
             // printing columns:
-            num++;
+            
             int p = num ; 
             for (int j = 0; j <= i; j++) {
                 System.out.print(p + " ");
                 p--;
                 num++ ; 
             }
-
+            num++; // this process has to be done because num inside the for loop is already incrasing the number of times that loop is running
+                    // but the next loop has to run to one more than the previos row number, so number has to be updated for the next row, ie add one 
+                    // for example, in third row, using num++, num would be increased only 3 times, but the next row has to go on for 4 times, hence num++ after ending for loop 
 
             // Moving cursor to next row
             System.out.println();
