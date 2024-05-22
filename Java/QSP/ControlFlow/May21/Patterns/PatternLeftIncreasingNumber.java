45b964e2c5228891e063e3f59b8c7209ec6a6d85
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-22 11:18:19 +0530

    success

--- Java/QSP/ControlFlow/May21/Patterns/PatternLeftIncreasingNumber.java
+++ Java/QSP/ControlFlow/May21/Patterns/PatternLeftIncreasingNumber.java
@@ -2,22 +2,24 @@
 public class PatternLeftIncreasingNumber {
     public static void main(String[] args) {
         int n = 5;
-        int diff = 1;
-        int num = 1;
+        int num = 0;
+
+
         for (int i = 0; i < n; i++) {
 
             // printing columns:
-            int start = num + i - 1;
-
+            num++;
+            int p = num ; 
             for (int j = 0; j <= i; j++) {
-                System.out.print(start + " ");
-                start--;
+                System.out.print(p + " ");
+                p--;
+                num++ ; 
             }
+
 
             // Moving cursor to next row
             System.out.println();
-            diff++;
-            num = num + diff;
+            
 
         }
     }
