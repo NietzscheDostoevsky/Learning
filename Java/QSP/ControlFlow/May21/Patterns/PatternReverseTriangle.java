a1a94c2bdee49ef64edb4da456c2bc414122df7b
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-22 08:54:31 +0530

    reformat

--- Java/QSP/ControlFlow/May21/Patterns/PatternReverseTriangle.java
+++ Java/QSP/ControlFlow/May21/Patterns/PatternReverseTriangle.java
@@ -1,20 +1,19 @@
-import java.util.Scanner ; 
+import java.util.Scanner;
 
-class PatternReverseTriangle{
+class PatternReverseTriangle {
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in) ; 
+
+		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the number to stars: ");
-		int num = sc.nextInt() ; 
-		
-		
-		for(int i = num ; i>=1 ; i--){
-			
-			for(int j = 1 ; j<=i; j++){
+		int num = sc.nextInt();
+
+		for (int row = num; row >= 1; row--) {
+
+			for (int col = 1; col <= row; col++) {
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
-		
+
 	}
 }
