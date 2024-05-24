fe7d7dad27eaadaf132ce0e9bd1e109bc31bd25d
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-24 12:05:05 +0530

    inverse pyramid done

--- Java/QSP/ControlFlow/May23/PatternPrinter.java
+++ Java/QSP/ControlFlow/May23/PatternPrinter.java
@@ -7,11 +7,12 @@ public class PatternPrinter {
         // System.out.print("Enter the number to rows: ");
         // int input = sc.nextInt();
         // int n = input ;
-        int n = 7;
+        int n = 9;
 
-        // printSquarePattern(num);
         pyramidPattern(n);
         System.out.println();
+
+        inversePyramid(n);
         System.out.println();
 
     }
@@ -22,23 +23,38 @@ public class PatternPrinter {
 
         // outer loop to print the rows
         for (int i = 0; i < n; i++) {
-
             // inner loop to print the columns
 
             // first loop to print the leading blanks
-            for (int j = 0; (i + j) < (n); j++) {
+            for (int j = 0; (i + j) < (n - 1); j++) {
                 System.out.print(" ");
             }
             // Second loop to print the stars
-
             for (int k = 1; k <= (i * 2 + 1); k++) {
                 System.out.print("*");
-
             }
-
             System.out.println(); // poi9nting the cursonr t next line
         }
 
+    }
+
+    public static void inversePyramid(int n) {
+        // printing inverse pyramid
+        int p = n;
+        // outer loop to print the columns
+        for (int i = 0; i < n; i++) {
+
+            // first iner loop to print the spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // second loop to print stars
+            for (int k = (2 * p - 1); k > 0; k--) {
+                System.out.print("*");
+            }
+            p--;
+            System.out.println();
+        }
     }
 
 }
