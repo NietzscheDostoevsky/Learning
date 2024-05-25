4394fba22158f9f6f680b8b5e04b1b1bdb4d2b7c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-25 10:07:42 +0530

    added alphanumbericsqaure

--- Java/QSP/ControlFlow/May24/Patterns.java
+++ Java/QSP/ControlFlow/May24/Patterns.java
@@ -3,7 +3,9 @@ class Patterns{
 		
 		int n = 9 ; 
 
-
+		SquareDiagonal(n);
+		System.out.println() ; 
+		AlphanumericSquare(); 
 
 	}
 
@@ -11,10 +13,35 @@ class Patterns{
 
 		for(int i = 0; i < n ; i++){
 			for(int j = 0; j < n ; j++){
-				System.out.print("*"); 
+				if(i == 0 || i == n-1 || j == 0 || j == n-1 || i == j || i+j == n-1){
+					System.out.print("* "); 
+				}else{
+					System.out.print("  ");
+				}
 			}
+			System.out.println();
 
-		}
-			
+		}		
 	}
+
+	public static void AlphanumericSquare() {
+
+		int n = 4 ; 
+		char alpha = 'a' ; 
+		int num = 8 ;
+
+		for (int i = 0 ; i < n ; i++) {
+			for(int j = 0 ; j< n; j++){
+				if(i%2 == 0){
+					System.out.print(alpha); 
+					alpha++; 
+				}else{
+					System.out.print(num); 
+					num--;
+				}
+			}
+			System.out.println();
+		}
+	}
+
 }
