5230ecfb3abc8b07ece67962ca8dc4cf83d75bd9
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-27 19:21:55 +0530

    Update Password.java

--- Java/QSP/ControlFlow/May27/Password.java
+++ Java/QSP/ControlFlow/May27/Password.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 class Password{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
 		int pin1 = 143 ;
-		int attempt = 3 ;
+		int duration = 2000;
 
-		do{
+		outerloop:
+		for(;;){
+			int attempt = 3 ;
+			do{
 			System.out.print("Enter the pin: ");
 			int pin2 = sc.nextInt(); 
 			
 			if(pin1 == pin2){
 				System.out.println("Phone Unlocked");
-				break;
+				break outerloop;
+
 			}else{
 				System.out.println("Wrong Pin! " + "Attempt left " + (attempt -1));
 				if((attempt-1)==0){
 					System.out.println("Try after some time");
+					Thread.sleep(duration);
 				}
 			}
 
 			attempt--;
-		}while(attempt>0);
+			}while(attempt>0);
+		}
 
 	}
 }
