41ee18edf930e16d83bb5235ff9b11890860e1e4
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-27 14:36:11 +0530

    Create ex7.c

---
+++ C/codehardway/ex7/ex7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	float power = 2.345f;
+	double super_power = 56789.4532;
+	char initial = 'A';
+	char first_name[] = "Saurabh";
+	char last_name[] = "Singh";
+	
+	printf("You are %d miles away.\n", distance);
+}
