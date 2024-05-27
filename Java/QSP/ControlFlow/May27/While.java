9300caf2f2a32893e725131e5bd999e4e093e94a
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-27 16:40:20 +0530

    while example

---
+++ Java/QSP/ControlFlow/May27/While.java
@@ -0,0 +1,13 @@
+class While{
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		do{
+			System.out.println("helloP");
+			i++; 
+		}while(false); 
+
+		System.out.print("OUtside");
+	}
+}
