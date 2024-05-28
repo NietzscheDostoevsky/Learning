acbd7aafc79fcc6452832662a324bcfc58a257e7
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-28 16:52:46 +0530

    success

--- Java/QSP/ControlFlow/May28/Peterson.java
+++ Java/QSP/ControlFlow/May28/Peterson.java
@@ -8,6 +8,29 @@ class Peterson{
 		Scanner sc = new Scanner(System.in); 
 		System.out.print("Enter the number : ");
 		int num = sc.nextInt(); 
-		
+
+		int originalNum = num ; 
+		int result = 0; 
+
+		// LOGIC 
+
+		while(originalNum != 0){
+			int lastDigit = originalNum%10; 
+			int fact = 1; 
+			for(int i = 1; i<= lastDigit; i++){
+				fact = fact * i ; 
+			}
+
+			result = result + fact ; 
+			originalNum = originalNum / 10 ; 
+		}
+
+		if(result == num){
+			System.out.println("Peterson Number");
+		}else{
+			System.out.println("Not a peterson number");
+		}
+
+
 	}
 }
