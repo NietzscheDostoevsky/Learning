a6ca16c52ab725116fae40a4d7caa997d1706735
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-28 19:45:32 +0530

    Create ex13.c

---
+++ C/codehardway/ex13/ex13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	if(argc !=2)
+	{
+		printf("ERROR: You need one argument. \n"); 
+		// this is how you abort a programme 
+		return 1;
+	}
+
+	int i = 0 ; 
+	for(i = 0; argv[1][i] != '\0'; i++)
+	{
+		char letter = argv[1][i] ; 
+
+		switch(letter) 
+		{
+			case 'a': 
+			case 'A':
+				printf("%d: 'A'\n", i); 
+				break;
+			case 'e':
+			case 'E':
+
+		}
+	}
+}
