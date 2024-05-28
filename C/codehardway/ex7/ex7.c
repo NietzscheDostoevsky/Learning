ce9d857e82ce5290f2d2fe8d063b24b1a8e4ed77
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-28 09:22:20 +0530

    caused integer overflow error

--- C/codehardway/ex7/ex7.c
+++ C/codehardway/ex7/ex7.c
@@ -8,41 +8,43 @@ int main(int argc, char *argv[])
 	char initial = 'K';
 	char first_name[] = "Saurabh";
 	char last_name[] = "Singh";
-	
+
 	printf("You are %d miles away.\n", distance);
 	printf("You have %f leves of power.\n", power);
 	printf("You have %f awesome super pwoers.\n", super_power);
 	printf("I have an initial %c. \n", initial);
 	printf("I have a first name %s. \n", first_name);
-	printf("I have a last name %s. \n", last_name); 
-	printf("My whole name is %s %c. %s. \n", 
-			first_name, initial,
-			last_name );
-	
-	int bugs = 100 ;
-	double bug_rate = 1.2; 
-	printf("You have %d bugs at the imaginary rate of %f. \n", 
-			bugs, bug_rate);
+	printf("I have a last name %s. \n", last_name);
+	printf("My whole name is %s %c. %s. \n",
+		   first_name, initial,
+		   last_name);
 
-	long universe_of_defects = 1L * 1024L * 1024L * 1024L; 
-	printf("The entire universe has %ld bugs.\n", 
-			universe_of_defects); 
+	int bugs = 100;
+	double bug_rate = 1.2;
+	printf("You have %d bugs at the imaginary rate of %f. \n",
+		   bugs, bug_rate);
 
-	double expected_bugs = bugs * bug_rate ; 
-	printf("You are expected to have %f bugs \n", 
-			expected_bugs) ; 
+	long universe_of_defects = 1L * 1024L * 1024L * 1024L * 999999999999999L; // caused integer overflow error
+	printf("The entire universe has %ld bugs.\n",
+		   universe_of_defects);
 
-	double part_of_universe = expected_bugs / universe_of_defects ;
-	printf("That is only a %e portion of the universe. \n", 
-			part_of_universe); 
+	double expected_bugs = bugs * bug_rate;
+	printf("You are expected to have %f bugs \n",
+		   expected_bugs);
 
+	double part_of_universe = expected_bugs / universe_of_defects;
+	printf("That is only a %e portion of the universe. \n",
+		   part_of_universe);
 
 	// this makes no sense, just a some of something weird
-	
-	char nul_byte = '\0'; 
-	int care_percentage = bugs * nul_byte ; 
-	printf("Which means you should care %d%%.\n",
-			care_percentage); 
 
-	return 0 ; 
+	char nul_byte = '\0';
+	int care_percentage = bugs * nul_byte;
+	printf("Which means you should care %d%%.\n",
+		   care_percentage);
+	// See what happens if you try to print out the nul_byte variable
+	// along with % s versus % c.
+	printf("This is testing null character 0 : %c , %s \n\n", nul_byte, nul_byte);
+
+	return 0;
 }
