344b5b80eddb31c502840e9551282e61e79e7e0d
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-28 09:55:29 +0530

    reformat and fixed initialization error

--- C/codehardway/ex8/ex8.c
+++ C/codehardway/ex8/ex8.c
@@ -3,19 +3,25 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	
-	if (argc == 1) {
+
+	if (argc == 1)
+	{
 		printf("You have only one argument. \n");
-	} else if (argc > 1 && argc <4) {
+	}
+	else if (argc > 1 && argc < 4)
+	{
 		printf("Here's your arguments: \n");
-		
-		for (i = 0;i<argc; i++) {
-			printf("%s ", argv[i]); 
+
+		for (i = 1; i < argc; i++)
+		{
+			printf("%s ", argv[i]);
 		}
 		printf("\n");
-	} else {
-		printf("You have too many arguments.\n"); 
 	}
-	
-	return 0 ; 
+	else
+	{
+		printf("You have too many arguments.\n");
+	}
+
+	return 0;
 }
