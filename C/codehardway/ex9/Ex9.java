293efe3ab7e42f6da97444d0bce14d800cf1073f
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-28 10:23:51 +0530

    same code with c, java and python

---
+++ C/codehardway/ex9/Ex9.java
@@ -0,0 +1,10 @@
+class Ex9 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 25) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+    }
+}
