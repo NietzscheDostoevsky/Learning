d1b7cb26a9f1de5daf546f3b27f3621434a002b9
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-05-28 16:36:54 +0530

    Create Peterson.java

---
+++ Java/QSP/ControlFlow/May28/Peterson.java
@@ -0,0 +1,13 @@
+// Check if the number is Peterson number 
+
+import java.util.Scanner; 
+
+class Peterson{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+		System.out.print("Enter the number : ");
+		int num = sc.nextInt(); 
+		
+	}
+}
