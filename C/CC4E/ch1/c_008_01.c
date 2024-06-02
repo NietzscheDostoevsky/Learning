d2dbda466d69721309e80e394b367cfe3a99fe36
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-02 23:46:09 +0530

    rename

--- C/CC4E/ch1/c_008_01.c
+++ C/CC4E/ch1/c_008_01.c
@@ -10,10 +10,11 @@ main()
 	upper = 300 ; // upper limit 
 	step = 20 ; 
        	fahr = lower ; 
-
+	printf("Fahr  Celcius \n");	
+	
 	while (fahr<= upper)
 	{
-		celsius = (5.0/9.0) * (fahr - 32 ) ; 
+		celsius = (5.0/9.0) * (fahr - 32 ) ;
 		printf("%4.0f %6.1f \n", fahr, celsius);
 		fahr = fahr + step ;
 	}	
---
+++ C/CC4E/ch1/c_011_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+	int fahr; 
+	printf("Fahr  Celsius\n");
+
+	for( fahr = 0; fahr <= 300; fahr = fahr + 20 )
+	{
+		printf("%4.0d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	}
+}
+
