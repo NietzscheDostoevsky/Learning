ed640bc37ac4a62cde345e73ef6f40a16d0958ca
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-04 17:49:32 +0530

    arithmatic examples using methods

---
+++ Java/QSP/ControlFlow/May29/ArithmaticOperations.java
@@ -0,0 +1,69 @@
+import java.util.Scanner; 
+class ArithmaticOperations{
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	public static void addition(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter num1: ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter num2: ");
+		int num2 = sc.nextInt(); 
+
+		int out = num1 + num2 ; 
+		System.out.println(num1 + " + " + num2 + " = " + out) ; 
+			
+	}
+
+	public static void substraction(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter num1: ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter num2: ");
+		int num2 = sc.nextInt(); 
+
+		int out = num1 - num2 ; 
+		System.out.println(num1 + " - " + num2 + " = " + out) ; 
+			
+	}
+
+	public static void multiplication(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter num1: ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter num2: ");
+		int num2 = sc.nextInt(); 
+
+		int out = num1 * num2 ; 
+		System.out.println(num1 + " X " + num2 + " = " + out) ; 
+			
+	}
+
+	public static void division(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter num1: ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter num2: ");
+		int num2 = sc.nextInt(); 
+
+		int out = num1 / num2 ; 
+		System.out.println(num1 + " / " + num2 + " = " + out) ; 
+			
+	}
+
+	public static void mod(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter num1: ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter num2: ");
+		int num2 = sc.nextInt(); 
+
+		int out = num1 % num2 ; 
+		System.out.println(num1 + " % " + num2 + " = " + out) ; 
+			
+	}
+
+}
