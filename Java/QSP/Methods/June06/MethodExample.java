bc8a79bdd18b78cea66c563670f5f8eb368a0602
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-06 20:03:04 +0530

    success

---
+++ Java/QSP/Methods/June06/MethodExample.java
@@ -0,0 +1,32 @@
+class SumOfPrimeDigit{
+	public static void main(String[] args) {
+		int num = 123456 ; 
+		System.out.println(isPrime(13));
+		int sum = 0 ; 
+		while(num>0){
+			int rem = num%10 ; 
+			if(isPrime(rem)){
+				sum = sum + rem ; 
+			}
+			num = num / 10 ; 
+		}
+		System.out.println(sum);
+		
+
+	}
+
+	public static boolean isPrime(int num){
+		
+		if(num<2)
+			return false;
+		if(num==2)
+			return true ;
+		
+		for( int i = 3; i*i<=num; i= i+2){
+			if(num%i == 0){
+				return false;
+			}
+		}
+		return true ; 
+	}
+}
