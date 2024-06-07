50596c3430e192bc84fb7a5bbc1dad3957456665
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-07 15:59:58 +0530

    Update MethodExample.java

--- Java/QSP/Methods/June06/MethodExample.java
+++ Java/QSP/Methods/June06/MethodExample.java
@@ -29,4 +29,18 @@ class SumOfPrimeDigit{
 		}
 		return true ; 
 	}
+
+		public static boolean IsPrime(int num) {
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }	
 }
