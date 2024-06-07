fb595ddcce4dcf26d9af74b399f5c8023fdcb1b9
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-07 18:45:24 +0530

    success

--- Java/QSP/Methods/June06/BuzzNumber.java
+++ Java/QSP/Methods/June06/BuzzNumber.java
@@ -1,3 +1,20 @@
+import java.util.Scanner ;
+
 class BuzzNumber{
+
 	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in); 
+		System.out.print("Enter the number: ");
+		int num = sc.nextInt(); 
+
+		if((num%7 == 0) || ((num%10) == 7)){
+			System.out.println("BuzzNumber");
+		}else{
+			System.out.println("Not a Buzz number");
+		}
+		
+	}
+
 }
