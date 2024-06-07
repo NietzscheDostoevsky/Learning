501384be050e3cb7c13643bf26079cf3e052c4d1
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-07 12:14:27 +0530

    next terms

--- Java/QSP/Methods/June06/KeithNumber.java
+++ Java/QSP/Methods/June06/KeithNumber.java
@@ -16,6 +16,30 @@ class KeithNumber{
 
 	public static void main(String[] args) {
 		
-		int num = 197 ; 
+		int num = 197 ;
+        int num2 = num ; 
+		int len = length(num);
+		System.out.println("num: " + num);
+		System.out.println("len :" + len);
+
+		int numSum = 0 ; 
+
+		for(int i = 1; i<=len; i++){
+
+			System.out.println(num2%10);
+			numSum +=  num2%10 ; 
+			num2/=10; 
+		}
+		System.out.println("numsum:" + numSum);
+
+        int next = 0  ; 
+        for(int i = 1 ; i<=(len-1); i++){
+            next += num% 10 ; 
+            num /= 10 ; 
+        }
+        System.out.println(next);
+        next = next + numSum ; 
+        System.out.println("next: " + next);
 	}
 
+}
