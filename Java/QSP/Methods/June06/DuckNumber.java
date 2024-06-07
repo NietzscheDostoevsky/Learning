236332a3c886bd7bb5b0dd01b922a9179de69be1
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-07 19:08:50 +0530

    success

--- Java/QSP/Methods/June06/DuckNumber.java
+++ Java/QSP/Methods/June06/DuckNumber.java
@@ -1,1 +1,24 @@
-DuckNumber
+class DuckNumber{
+	public static void main(String[] args) {
+		
+		int num = 1203; 
+		System.out.println(num);
+
+		 boolean flag = false ; 
+
+		 int i = num ; 
+		 while(i!=0){
+		 	int rem = i%10 ; 
+		 	if(rem == 0) 
+		 		flag = true ;
+		 	i /= 10 ; 
+		 }
+
+		 if(flag){
+		 	System.out.println("Duckkk");
+		 }else{
+		 	System.out.println("NOT a DUcky");
+		 }
+	}
+
+}
