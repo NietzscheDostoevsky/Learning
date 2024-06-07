53f2b875a7f0ef7a3d5881efc6679ac59e492363
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-07 11:21:49 +0530

    success

--- Java/QSP/Methods/June06/CircularPrime.java
+++ Java/QSP/Methods/June06/CircularPrime.java
@@ -4,11 +4,28 @@ class CircularPrime{
 
 		int length = 0 ; 
 
+		while(num!=0){
+
+			length++ ;  
+			num = num / 10  ; 
+		}
+
 		return length ; 
 	}
 
-	public static int CyclincPermutation(int num){
+	public static int cyclicPermutation(int num){
+		// 1234 -> 2341 ; 2341 -> 3412 ; 
 
+		int next = 0 ; 
+
+		int multiplier = 1; 
+		for(int i = 1; i<length(num); i++){
+			multiplier = multiplier * 10 ; 
+		} 
+		next = num % multiplier;
+		int digit = num / multiplier ;
+
+		next = next * 10 + digit ;
 		return next ;
 	}
 
@@ -28,8 +45,26 @@ class CircularPrime{
 	}
 
 	public static void main(String[] args) {
+
+		boolean flag = true ;
+		int num = 19937 ;
+		int next ; 
 		
-		int num = 1193 ;
+		for(int i = 1; i<length(num); i++){
+			next = cyclicPermutation(num) ; 
+			System.out.print(next + "  : "); 
+			System.out.println(isPrime(next)); 
+			flag = flag && isPrime(next);
+			num = next ;
+
+
+		}
+
+		if(flag)
+			System.out.println("Circular Prime");
+		else
+			System.out.println("NOT Circular Prime"); 
+
 
 	}
 }
