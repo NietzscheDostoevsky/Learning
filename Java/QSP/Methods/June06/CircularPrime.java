82977ea7e3a57aaa80522dac0678ea3ae769d15c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-07 10:35:07 +0530

    methods

--- Java/QSP/Methods/June06/CircularPrime.java
+++ Java/QSP/Methods/June06/CircularPrime.java
@@ -1,1 +1,25 @@
-CircularPrime
+class CircularPrime{
+
+	public static int length(int num){
+
+		int length = 0 ; 
+
+		return length ; 
+	}
+
+	public static int CyclincPermutation(int num){
+
+		return next ;
+	}
+
+	public static boolean isPrime(int num){
+
+		boolean flag = false ; 
+		return flag ; 
+	}
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
