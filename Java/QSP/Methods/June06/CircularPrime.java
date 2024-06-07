fab0d15254bc9e9e086fd969078cf996d6e31f6e
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-07 10:53:28 +0530

    isPrime implemenmtation

--- Java/QSP/Methods/June06/CircularPrime.java
+++ Java/QSP/Methods/June06/CircularPrime.java
@@ -14,12 +14,22 @@ class CircularPrime{
 
 	public static boolean isPrime(int num){
 
-		boolean flag = false ; 
-		return flag ; 
+		
+		if(num <=1 ) return false ;
+		if(num == 2 ) return true ; 
+		if(num%2 == 0) return false ; 
+
+		for(int i = 3; i*i<=num; i+=2){
+			if(num%i == 0)
+				return false ; 
+		}
+
+		return true  ; 
 	}
 
 	public static void main(String[] args) {
 		
-		
+		int num = 1193 ;
+
 	}
 }
