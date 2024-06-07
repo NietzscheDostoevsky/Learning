00cae4083898573baf50316985bed49f20cd9337
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-07 18:30:07 +0530

    basic structure

--- Java/QSP/Methods/June06/ISBN.java
+++ Java/QSP/Methods/June06/ISBN.java
@@ -1,3 +1,22 @@
 class ISBN{
-	System.out.println();
+	
+	// verify ISBN number is valid or not
+
+	public static boolean isISBN(long num){
+
+		return false ;
+	}
+
+	public static void main(String[] args){
+
+		long num = 1259060977l ; 
+		long num2 = 8147852369l ;
+
+		if(isISBN(num))
+			System.out.println(num + " : Is leagl ISBN");
+		else
+			System.out.println(num + " : Is not a legal ISBN");
+	}
+
+
 }
