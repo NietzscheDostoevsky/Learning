b20929359fd038e4a150cf12b485343f51bfc5a3
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-14 17:48:49 +0530

    arithmatic opes

---
+++ Java/QSP/OOPS/June14/StatMember.java
@@ -0,0 +1,22 @@
+class StatMember{
+
+	public static void main(String[] args) {
+		
+
+	}
+}
+
+class ArithmaticOperation{
+
+	public static int addition(int num1, int num2){
+		return num1 + num2;
+	}
+
+	public static int substraction(int num1, int num2){
+		return num1 - num2 ; 
+	}
+
+	public static int multiplicatiion(int num1, int num2){
+		return num1 * num2 ; 
+	}
+}
