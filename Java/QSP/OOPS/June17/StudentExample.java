c2bb50f235229dc43b4803264b51aacabd1e080e
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-17 19:00:20 +0530

    class example

---
+++ Java/QSP/OOPS/June17/StudentExample.java
@@ -0,0 +1,42 @@
+class Student {
+	String sname; 
+	String nplace ;
+	String education;
+	String branch; 
+	int yop;
+	double cgpa;
+	long contactNumber;
+
+	public void displayStudentDetails(){
+		System.out.println("*** Student Info ****");
+		System.out.println("Name: " + sname);
+		System.out.println("Native Place: " + nplace);
+		System.out.println("Education: " + education);
+		System.out.println("Branch: " + branch);
+		System.out.println("Year of Passout: " + yop);
+		System.out.println("CGPA: " + cgpa);
+		System.out.println("Contact Number : " + contactNumber);
+		System.out.println();
+	}
+}
+
+class StudentDriver {
+	// driver method always contains the main method 
+	// it drives the executino 
+	public static void main(String[] args) {
+		Student obj = new Student();
+		System.out.println(obj);
+		// obj.sname = "Jayasurya";
+		// obj.nplace = "Kohlapur"; 
+		// obj.education = "BCA"; 
+		// obj.branch = "Computer Science"; 
+		// obj.yop = 2023; 
+		// obj.cgpa = 7.77; 
+		// obj.contactNumber = 9898989898l; 
+
+		obj.displayStudentDetails(); 
+
+		 
+
+	}
+}
