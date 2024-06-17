53161c1c5184a58dc31841ce56990bd26d8c01cc
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-17 19:01:44 +0530

    Update Stack.java

---
+++ Java/javaReference/chapter07/Stack.java
@@ -0,0 +1,10 @@
+// Integer stack that can contains 10 values. 
+
+class Stack {
+	/*
+	making stck and tos as private. This measns that
+	they cannot be accidently or malicoiusly altered in a way taht would be harmful
+	to the stack. 
+	*/
+	
+	private 
