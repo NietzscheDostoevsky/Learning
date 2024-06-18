a3c9cbae21db851499c0d950e04275b8154c2ba5
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-18 10:06:56 +0530

    changed formatting

--- Java/javaReference/chapter07/Stack.java
+++ Java/javaReference/chapter07/Stack.java
@@ -2,40 +2,36 @@
 
 class Stack {
 	/*
-	making stck and tos as private. This measns that
-	they cannot be accidently or malicoiusly altered in a way taht 
-	would be harmful to the stack. 
-	*/
+	 * making stck and tos as private. This measns that
+	 * they cannot be accidently or malicoiusly altered in a way taht
+	 * would be harmful to the stack.
+	 */
 	// private means this code can only be accessed by the other members
-	// of this class. 
-	private int[] stck = new int[10]; 
+	// of this class.
+	private int[] stck = new int[10];
 	private int tos; // top of stack
-	
 
-	//initialize top of stack
-	Stack(){
-		tos = -1; 
+	// initialize top of stack
+	Stack() {
+		tos = -1;
 	}
 
-	//push an item into the stack 
+	// push an item into the stack
 	void push(int item) {
-		if(tos==9) {
+		if (tos == 9) {
 			System.out.println("Stack if full");
-		}
-		else {
-			stck[++tos] = item; 
+		} else {
+			stck[++tos] = item;
 		}
 	}
 
-	// pop an item from stack 
+	// pop an item from stack
 	int pop() {
-		if(tos<0) {
+		if (tos < 0) {
 			System.out.println("Stack underflow");
-			return 0; 
-		}
-		else {
+			return 0;
+		} else {
 			return stck[tos--];
 		}
 	}
 }
-
