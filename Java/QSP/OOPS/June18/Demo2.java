4deb9025974a2688aebe5e4fd0de3d42b129e88b
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-18 21:05:10 +0530

    Update Demo2.java

---
+++ Java/QSP/OOPS/June18/Demo2.java
@@ -0,0 +1,18 @@
+class Demo2 {
+	static String str = "GLOBAL"; 
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+
+	public void m1(){
+		System.out.println("Ho from m1 of demo2 class");
+	}
+
+	public static void m2(){
+		System.out.println("static m2()");
+	}
+}
+
+class Demo3{
+	
+}
