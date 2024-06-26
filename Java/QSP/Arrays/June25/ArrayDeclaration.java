a377fa43ede1fb885d665996182992c92d41e370
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-26 16:24:28 +0530

    array declaration

---
+++ Java/QSP/Arrays/June25/ArrayDeclaration.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class ArrayDeclaration {
+	public static void main(String[] args) {
+		
+		// Dexlare arrays in multiple ways
+
+		int a [] = new int [5];
+		int b [] = new int [6];
+		System.out.println(a);
+		System.out.println(b);
+
+
+
+
+	}
+}
