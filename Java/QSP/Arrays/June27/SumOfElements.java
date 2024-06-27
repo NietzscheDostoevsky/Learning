b035a0819b5f1c628ec59a400c456451af21f3f8
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-27 21:12:44 +0530

    Create SumOfElements.java

---
+++ Java/QSP/Arrays/June27/SumOfElements.java
@@ -0,0 +1,20 @@
+class SumOfElements{
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		int sum = 0;
+		for(int i: arr) {
+			
+			if(i%2==0) sum+=i;
+		}
+		System.out.println(sum);
+
+		int[] arr2 = {1,0,-1, -2,3, -10, 4, -7};
+		
+		
+
+		
+
+	}
+}
