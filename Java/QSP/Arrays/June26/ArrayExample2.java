75404f62e884fa8084e90961993e110f314c3612
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-27 21:13:13 +0530

    Update ArrayExample2.java

--- Java/QSP/Arrays/June26/ArrayExample2.java
+++ Java/QSP/Arrays/June26/ArrayExample2.java
@@ -30,34 +30,12 @@ class ArrayExample2 {
 		System.out.println(Arrays.deepToString(b));
 
 		int[][] c = { { 1, 2 },
-				{ 3, 4, 5, 6 } };
+					  { 3, 4, 5, 6 } };
 		System.out.println(Arrays.deepToString(c));
 
 		int[][][] d = {
 				{ { 10, 20, 30 } }, { { 40 }, { 50 } }, { { 60, 70 }, { 80, 90 }, { 100, 110 } } };
 		System.out.println(Arrays.deepToString(d));
-
-		int[][][] e = new int[3][][];
-		e[0] = new int[1][3];
-		e[1] = new int[2][1];
-		e[2] = new int[3][2];
-		System.out.println(Arrays.deepToString(e));
-
-		// a[0][0][0] = 10;
-		// a[0][0][1] = 20;
-		// a[0][0][2] = 30;
-		// a[1][0][0] = 40;
-		// a[1][1][0] = 50;
-		// a[2][0][0] = 60;
-		// a[2][0][0] = 60;
-		// a[2][0][0] = 60;
-
-		int[][][] f = new int[2][1][];
-		f[0][0] = new int[2];
-		f[1][0] = new int[4];
-
-		System.out.println(Arrays.deepToString(f));
-
 
 	}
 }
