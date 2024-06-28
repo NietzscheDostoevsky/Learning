b63fd56907c7e2446664bd347260227d07c3b1a2
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-28 10:07:08 +0530

    success

--- Java/QSP/Arrays/June28/ArrayPrint1D.java
+++ Java/QSP/Arrays/June28/ArrayPrint1D.java
@@ -1,16 +1,35 @@
 class ArrayPrint1D {
 	public static void main(String[] args) {
 
-		int[] arr = {1,2,3,4,5,6,7} ;
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
 
-		// for loop 
-		for(int i=0; i<arr.length;i++){
-			System.err.print(arr[i] + " ");
+		// for loop
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
-
+		System.out.println();
 		// while loop
-		
-		
 
+		int j = 0;
+		while (j < arr.length) {
+			System.out.print(arr[j] + " ");
+			j++;
+		}
+		System.out.println();
+
+		// do while
+		int k = 0;
+		do {
+			System.out.print(arr[k] + " ");
+			k++;
+		} while (k < arr.length);
+		System.out.println();
+
+		// for each
+
+		for (int m : arr) {
+			System.out.print(m + " ");
+		}
 	}
+
 }
