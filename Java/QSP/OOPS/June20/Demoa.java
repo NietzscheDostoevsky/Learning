546fbe0292db43b1881011860b39453d45e2158d
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-28 09:49:59 +0530

    success

---
+++ Java/QSP/OOPS/June20/Demoa.java
@@ -0,0 +1,14 @@
+class Demoa {
+	
+	String str; 
+	static Demoa obj = new Demoa(); 
+	public static void main(String[] args) {
+	
+	}
+	
+	{ 
+	str = "hi s1"; 
+	System.out.println(str); 
+	} 
+} 
+	
