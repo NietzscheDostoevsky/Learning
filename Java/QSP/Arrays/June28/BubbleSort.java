4b15b2ddea412939afe23aeaa175aa5c78e578ae
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-29 09:43:06 +0530

    Create BubbleSort.java

---
+++ Java/QSP/Arrays/June28/BubbleSort.java
@@ -0,0 +1,18 @@
+class BubbleSort {
+	public static void main(String[] args) {
+
+		int[] arr = {2,4,1,3, 2,3,4,1};
+		System.out.println(java.util.Arrays.toString(arr));
+
+		for(int i = 0; i<arr.length; i++){
+			for(int j = 0; j<arr.length; j++){
+				if(arr[i] > arr[j]){
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println(java.util.Arrays.toString(arr));
+	}
+}
