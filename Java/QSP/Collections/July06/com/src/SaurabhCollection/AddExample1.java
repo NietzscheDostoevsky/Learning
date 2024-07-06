0e608e88e3d9a75d0d86875454fb65b8f6e0d624
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-06 14:05:34 +0530

    ques

--- Java/QSP/Collections/July06/com/src/SaurabhCollection/AddExample1.java
+++ Java/QSP/Collections/July06/com/src/SaurabhCollection/AddExample1.java
@@ -1,13 +1,31 @@
 package SaurabhCollection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AddExample1 {
 	public static void main(String[] args) {
-		ArrayList<Integer> arrayList = new ArrayList<>();
-		for(int i = 10; i <=200; i+=10) {
-			arrayList.add(i);
-		}
-		System.out.println(arrayList);
+//		ArrayList<Integer> arrayList = new ArrayList<>();
+//		for(int i = 10; i <=200; i+=10) {
+//			arrayList.add(i);
+//		}
+//		System.out.println(arrayList);
+//		int[] arr = {1,2,3,4,5,6,7,8,9};
+//		ArrayList<Integer> arrayList = new ArrayList<>();
+//		for(Integer i:arr) {
+//			arrayList.add(i);
+//		}
+//		System.out.println(arrayList);
+		
+		
+		System.out.println;
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7);
+		System.out.println(list);
+		
+		int[] arr = {1,3,4,5,6} ;
+		List<int[]> list1 = new 
+		
 	}
 }
