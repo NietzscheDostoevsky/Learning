3aad1d18428597d9b8377df3d919004a1f1779b8
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-06 10:43:16 +0530

    Create AddAndAddAllExample.java

---
+++ Java/QSP/Collections/July06/com/src/SaurabhCollection/AddAndAddAllExample.java
@@ -0,0 +1,16 @@
+package SaurabhCollection;
+
+import java.util.*;
+
+public class AddAndAddAllExample {
+	public static void main(String[] args) {
+		Set set = new HashSet();
+		System.out.println(set.add(10));
+		System.out.println(set.add(20));
+		System.out.println(set.add("30"));
+		System.out.println(set.add(30));
+		System.out.println(set.add(40));
+		System.out.println(set.add(30));
+		
+	}
+}
