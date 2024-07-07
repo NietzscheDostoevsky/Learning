a10d6289b599b0dc117dfd65051a5b4709c2d5f4
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-07 08:43:52 +0530

    ques

--- Java/QSP/Collections/July06/com/src/SaurabhCollection/Shrikant.java
+++ Java/QSP/Collections/July06/com/src/SaurabhCollection/Shrikant.java
@@ -9,7 +9,7 @@ public class Shrikant {
 		
 		String str = "00110011";
 		
-		for(int i =0;i<str.length();i++)
+		for(int i =0;i<str.length()-1;i++)
 		{
 			if((str.charAt(i)=='1')&&(str.charAt(i+1)=='0')&& (str.lastIndexOf('0')>(str.charAt(i+1))&&(str.lastIndexOf('0'))!=str.indexOf('0')))
 			{
