c08155c538aa5441eb8625c77956254aa8f7578c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-07 08:43:59 +0530

    es

--- Java/QSP/Collections/July06/Demo.java
+++ Java/QSP/Collections/July06/Demo.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class Demo {
+	static TreeSet list = new TreeSet();
 	static ArrayList<Integer> list;
 	public static void main(String[] args) {
 		list.add(10); // null pointer exception 
