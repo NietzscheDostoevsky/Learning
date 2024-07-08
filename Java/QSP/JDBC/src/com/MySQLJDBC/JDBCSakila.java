6f62389e63a9c7d83d9f77aa87aa2e47a4fd5348
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-08 10:59:42 +0530

    Update JDBCSakila.java

--- Java/QSP/JDBC/src/com/MySQLJDBC/JDBCSakila.java
+++ Java/QSP/JDBC/src/com/MySQLJDBC/JDBCSakila.java
@@ -1,9 +1,26 @@
 package com.MySQLJDBC;
 
-public class JDBCSakila {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-	public static void main(String[] args) {
+public class JDBCSakila {
+	// to check java connectivity with mysql using sakila database
+
+	public static void main(String[] args) throws SQLException{
 		// TODO Auto-generated method stub
+		
+		String uname = "root";
+		String password = "";
+		String query = "SELECT * FROM SAKILA.ACTOR LIMIT 5";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
