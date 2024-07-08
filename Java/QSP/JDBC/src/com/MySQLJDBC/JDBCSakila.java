54c0d16481b968946387d349466d923f64d538f5
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-08 12:26:58 +0530

    success

--- Java/QSP/JDBC/src/com/MySQLJDBC/JDBCSakila.java
+++ Java/QSP/JDBC/src/com/MySQLJDBC/JDBCSakila.java
@@ -1,5 +1,6 @@
 package com.MySQLJDBC;
 
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -12,13 +13,31 @@ public class JDBCSakila {
 	public static void main(String[] args) throws SQLException{
 		// TODO Auto-generated method stub
 		
+		String url = "jdbc:mysql://localhost:3306/sakila";
 		String uname = "root";
-		String password = "";
+		String password = "Sss@12345#";
 		String query = "SELECT * FROM SAKILA.ACTOR LIMIT 5";
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			Connection con = DriverManager.getConnection(url, uname, password);
+			Statement statement = con.createStatement();
+			ResultSet result = statement.executeQuery(query);
+			
+			while(result.next()) {
+				String actorData = "";
+				for( int i = 1; i <=4; i++) {
+					actorData += result.getString(i) + "  "; 
+				}
+				System.out.println(actorData);
+			}
+			
+		}catch (SQLException e) {
 			e.printStackTrace();
 		}
 
