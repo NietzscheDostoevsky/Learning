1231bc3b60200af8d08caf0b7a02d0a36fecbcbd
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-09 15:00:40 +0530

    constructors are executed in order of derivation

--- Java/javaReference/chapter08/com/Inheritance/ConstructionExecutionOrder.java
+++ Java/javaReference/chapter08/com/Inheritance/ConstructionExecutionOrder.java
@@ -1,9 +1,33 @@
 package com.Inheritance;
 
+// Demostrate when concstructors are executetd 
+
+// creating a superclass
+
+class A {
+	A() {
+	System.out.println("Inside A's constructior");
+	}
+}
+
+// subclass by extending a 
+class B extends A {
+	B() {
+		System.out.println("Inside B's constructor");
+	}
+}
+
+class C extends B {
+	public C() {
+		System.out.println("Inside C's constructor ");
+	}
+}
+
 public class ConstructionExecutionOrder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		C c = new C(); 
 
 	}
 
