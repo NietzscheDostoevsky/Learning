719b1606a014ca6ace235af5538ac5c4b5e28537
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-09 14:54:53 +0530

    comtains method

---
+++ Java/QSP/Collections/July08/src/com/Saurabh/ContainsExample.java
@@ -0,0 +1,18 @@
+package com.Saurabh;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ContainsExample {
+	
+	public static void main(String[] args) {
+		
+		List list = Arrays.asList(10,20,30,40,50);
+		
+		for(int i = 10; i < =100 ; i+=10) {
+			list1
+		}
+	
+	}
+
+}
