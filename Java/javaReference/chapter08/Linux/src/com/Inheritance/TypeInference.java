cc636f5e0f40a3447cad010f56f606dc06e02ac5
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-09 21:56:17 +0530

    type inference using var

---
+++ Java/javaReference/chapter08/Linux/src/com/Inheritance/TypeInference.java
@@ -0,0 +1,12 @@
+package com.Inheritance;
+
+public class TypeInference {
+	
+	public static void main(String[] args) {
+		
+		var a = new int[10];
+		System.out.println(a.getClass().getName());
+		
+	}
+
+}
