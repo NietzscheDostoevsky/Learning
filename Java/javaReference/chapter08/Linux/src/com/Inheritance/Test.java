33582d596ea92cc8dc3753d2e3290fdcc74212f8
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-09 20:09:04 +0530

    new project

---
+++ Java/javaReference/chapter08/Linux/src/com/Inheritance/Test.java
@@ -0,0 +1,11 @@
+package com.Inheritance;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is testing ");
+
+	}
+
+}
