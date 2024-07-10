a1fbecc87fded18596cb31ecde33765d58479e00
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-10 17:30:00 +0530

    Create FixedDynamicStackdriver.java

---
+++ Java/javaReference/chapter08/InterfaceExamples/FixedDynamicStackdriver.java
@@ -0,0 +1,15 @@
+package InterfaceExamples;
+
+// Demonstrating how a interface varialbe can be used to 
+// refer to differnet classes that implements it 
+// dynamically at runtime
+
+
+public class FixedDynamicStackdriver {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
