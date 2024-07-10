2ef611fab53042402574d89449327dafc15ed22c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-10 12:14:14 +0530

    list interface

---
+++ Java/QSP/Collections/July10/src/com/ListInterface/Test.java
@@ -0,0 +1,24 @@
+package com.ListInterface;
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayList arr = new ArrayList(); 
+		arr.add("Kanchana"); 
+		arr.add(420); 
+		arr.add('c');
+		arr.add(4.5d) ;
+		arr.add(1.5f);
+		arr.add(true);
+		
+		for(int i = 0; i < arr.size(); i++) {
+			System.out.println(arr.get(i));
+		}
+		System.out.println(arr.indexOf(true));
+		
+
+	}
+
+}
