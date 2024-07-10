c30d88ac97c5f8af12783bde0fc237d0364d1454
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-10 22:07:21 +0530

    Update Test.java

--- Java/QSP/Collections/July10/src/com/ListInterface/Test.java
+++ Java/QSP/Collections/July10/src/com/ListInterface/Test.java
@@ -17,6 +17,23 @@ public class Test {
 			System.out.println(arr.get(i));
 		}
 		System.out.println(arr.indexOf(true));
+		ArrayList arr2 = new ArrayList(); 
+		
+		arr.add("Kanchana"); 
+		arr.add(420); 
+		arr.add('c');
+		arr.add(4.5d) ;
+		arr.add(1.5f);
+		arr.add(true);
+		
+		for(int i = 0; i < arr.size(); i++) {
+			System.out.println(arr.get(i));
+		}
+		System.out.println(arr.indexOf(true));
+		
+		
+		
+		
 		
 
 	}
