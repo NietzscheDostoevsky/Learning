e1137e19fdfb04fb2b56df76da039d70cfb69350
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-10 18:18:07 +0530

    add fish

--- Java/QSP/OOPS/July09/src/FishTank/Tank.java
+++ Java/QSP/OOPS/July09/src/FishTank/Tank.java
@@ -7,6 +7,8 @@ public class Tank {
 	String material; 
 	String shape;
 	double capacity;
+	Fish fish; 
+	Water water;
 	
 	public Tank(String dimensions, String material, String shape, double capacity) {
 		super();
@@ -20,6 +22,19 @@ public class Tank {
 	public String toString() {
 		return "Tank [dimensions=" + dimensions + ", material=" + material + ", shape=" + shape + ", capacity="
 				+ capacity + "]";
+	}
+	
+	public void addWater() {
+		water = new Water("Salt", 7, 9);
+		System.out.println("Water Added");				
+	}
+	
+	public void addFish() {
+		if(water == null) {
+			System.out.println("Add water first");
+		} else {
+			this.fish = new Fish("Shark", "Black", 50.0);
+		}		
 	}
 	
 }
