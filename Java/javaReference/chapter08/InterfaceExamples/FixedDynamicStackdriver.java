5e46b4a3004dede786955f4f6435ce53248244b7
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-11 12:05:01 +0530

    provided default clear() method for stack interface

--- Java/javaReference/chapter08/InterfaceExamples/FixedDynamicStackdriver.java
+++ Java/javaReference/chapter08/InterfaceExamples/FixedDynamicStackdriver.java
@@ -35,6 +35,8 @@ public class FixedDynamicStackdriver {
 		for(int i=0; i<8; i++) {
 			System.out.println(myStack.pop());
 		}
+		
+		myStack.clear();
 
 	}
 
--- Java/javaReference/chapter08/InterfaceExamples/IntStack.java
+++ Java/javaReference/chapter08/InterfaceExamples/IntStack.java
@@ -4,4 +4,11 @@ public interface IntStack {
 	public void push(int item); // store an item 
 	public int pop(); // retreive an item
 	
+	// providing a default clear() method for the stack interface
+	// without breaking the code
+	
+	default void clear() {
+		System.out.println("Clear is NOT implemented. ");
+	}
+	
 }
