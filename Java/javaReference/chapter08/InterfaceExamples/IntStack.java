ea8fee8cdbcc3184d91e36a9f6ac8fbaf4d01546
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-11 16:17:20 +0530

    added private method

--- Java/javaReference/chapter08/InterfaceExamples/IntStack.java
+++ Java/javaReference/chapter08/InterfaceExamples/IntStack.java
@@ -1,14 +1,42 @@
 package InterfaceExamples;
 
 public interface IntStack {
-	public void push(int item); // store an item 
+	public void push(int item); // store an item
+
 	public int pop(); // retreive an item
-	
+
 	// providing a default clear() method for the stack interface
 	// without breaking the code
-	
+
 	default void clear() {
 		System.out.println("Clear is NOT implemented. ");
 	}
-	
+
+	// A default method that returns an array that contans
+	// the next n elements on the stack after skipping
+	// elements.
+
+	default int[] popNElements(int n) {
+		// Return the requested elements.
+		return getElements(n);
+	}
+
+	// A default method that returns an array that contains
+	// the next n elements on the stack after skipping elements.
+	default int[] skipAndPopNElements(int skip, int n) {
+		// Skip the specified number of elements.
+		getElements(skip);
+		// Return the requested elements.
+		return getElements(n);
+	}
+
+	// A private method that returns an array containing
+	// the top n elements on the stack
+	private int[] getElements(int n) {
+		int[] elements = new int[n];
+		for (int i = 0; i < n; i++)
+			elements[i] = pop();
+		return elements;
+	}
+
 }
