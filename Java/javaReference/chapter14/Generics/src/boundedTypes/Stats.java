749729385552dd9a376dcd8535e79d9d8748bc46
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-13 08:30:20 +0530

    Create Stats.java

---
+++ Java/javaReference/chapter14/Generics/src/boundedTypes/Stats.java
@@ -0,0 +1,21 @@
+package boundedTypes;
+
+public class Stats<T extends Number> {
+	
+	T[] nums; // array of Number or a sublclass of Number 
+	Stats(T[] obj) { // defiining constructor
+		this.nums = obj;
+	}
+	
+	// return type double in all cases. 
+	double average() {
+		double sum = 0.0;
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i].doubleValue(); 
+		}
+		return sum / nums.length;
+	}
+	
+	
+	
+}
