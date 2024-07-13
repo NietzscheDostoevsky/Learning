137afe81906f31dbd0740da26309bcc323605dc9
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-13 17:36:19 +0530

    Create GenMathDemo.java

---
+++ Java/javaReference/chapter14/Generics/src/genericMethod/GenMathDemo.java
@@ -0,0 +1,15 @@
+package genericMethod;
+
+// demonstrate a simple generic method 
+
+public class GenMathDemo {
+	
+	
+		// Determine if an objectg is in array. 
+		
+		static <T extends Comparable<T>, V extends T> boolean isIn(T x, V[] y) {
+			
+		}
+	
+	
+}
