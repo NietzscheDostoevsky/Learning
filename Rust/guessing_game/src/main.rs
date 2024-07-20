57d1c6c99065a7f1ce644d22978e9a8d0634d240
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-20 20:07:29 +0530

    rust guessing game

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,13 +1,16 @@
-use std:io; 
+use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
-
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}")i;
     println!("Please input your guess"); 
 
     let mut guess = String::new(); 
 
-    io::stdid()
+    io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You guessed: {}", guess); 
