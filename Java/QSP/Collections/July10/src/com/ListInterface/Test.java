b62656973bf7b6c644e962e39fae14fa5780e0b4
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-20 23:57:37 +0530

    method . testing

--- Java/QSP/Collections/July10/src/com/ListInterface/Test.java
+++ Java/QSP/Collections/July10/src/com/ListInterface/Test.java
@@ -5,13 +5,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		ArrayList arr = new ArrayList(); 
+		ArrayList<Object> arr = new ArrayList<>(); 
 		arr.add("Kanchana"); 
 		arr.add(420); 
 		arr.add('c');
 		arr.add(4.5d) ;
 		arr.add(1.5f);
 		arr.add(true);
+		arr.add("hello");
 		
 		for(int i = 0; i < arr.size(); i++) {
 			System.out.println(arr.get(i));
@@ -24,7 +25,8 @@ public class Test {
 		arr.add('c');
 		arr.add(4.5d) ;
 		arr.add(1.5f);
-		arr.add(true);
+		arr
+			.add(true);
 		
 		for(int i = 0; i < arr.size(); i++) {
 			System.out.println(arr.get(i));
