18aecbca27ea073cd20dfeb6dc646b0cc58560c8
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-31 22:55:49 +0530

    private interface methods

---
+++ Java/javaReference/chapter09/PrivateMethod.java
@@ -0,0 +1,26 @@
+interface PrivateMethod {
+	
+		void push(int item); // store an item
+
+		int pop();
+
+		default int[] popNElements(int n) {
+
+			return getElements(n);
+		}
+
+		default int[] skipAndPopNElements(int skip, int n) {
+
+			getElements(skip);
+			return getElements(n);
+		}
+
+		private int[] getElements(int n) {
+			int[] elements = new int[n];
+			for (int i = 0; i < n; i++)
+				elements[i] = pop();
+			return elements;
+		}
+	
+
+}
