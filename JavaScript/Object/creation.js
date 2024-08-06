80018adca35054ab016b870c0bdb87fb59a73cfc
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-06 15:44:13 +0530

    js object creation using literals

---
+++ JavaScript/Object/creation.js
@@ -0,0 +1,8 @@
+let car = { 
+        brand: "TATA",
+        model : "Harrier", 
+        price : 120000,
+       
+};  
+        
+console.log(`Car company is ${car.brand} and model name is ${car.model} and car price is ${car.price}`);
