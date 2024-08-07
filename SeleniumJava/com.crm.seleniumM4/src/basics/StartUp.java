4c5389838a770a0a791e1956ba674da7404d80fc
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-07 09:15:36 +0530

    testig browser startup

---
+++ SeleniumJava/com.crm.seleniumM4/src/basics/StartUp.java
@@ -0,0 +1,22 @@
+package basics;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StartUp {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		Thread.sleep(5000);
+	    //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.close(); 
+		
+
+	}
+
+}
