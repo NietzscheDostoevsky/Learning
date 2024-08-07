30ce48f09edbd49b52031a5191d03ae66256bab8
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-07 17:45:50 +0530

    lambda reverse string

--- Java/javaReference/Chapter15/src/LambdaExpressionExamples/BlockLambdaDemo2.java
+++ Java/javaReference/Chapter15/src/LambdaExpressionExamples/BlockLambdaDemo2.java
@@ -1,10 +1,27 @@
 package LambdaExpressionExamples;
 
+// A block lambda that reverses the characters of a string. 
+
+@FunctionalInterface
+interface StringFunc {
+	String func(String n);
+}
+
 public class BlockLambdaDemo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// This block lambda reverses the characters in a string; 
+		StringFunc reverse = (str) -> {
+			String result = ""; 
+			int i;
+			
+			for (i = str.length()-1; i >= 0; i--)
+				result += str.charAt(i); 
+			return result; 
+		};
+		
+		System.out.println(String.format("Lambda reversed is: %s", reverse.func("Lambda")));
+		System.out.println(String.format("Expression reversed is: %s", reverse.func("Expression")));
 	}
-
 }
