9ff3387dc34d1587e65176b6a9e24f4c94162112
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-07 10:32:48 +0530

    going forward and backward in webpage

---
+++ SeleniumJava/com.crm.seleniumM4/src/basics/MoveForwardBackward.java
@@ -0,0 +1,22 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MoveForwardBackward {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("https://demowebshop.tricentis.com/");
+		Thread.sleep(3000);
+		driver.get("https://google.com");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+		driver.close();
+	}
+}
