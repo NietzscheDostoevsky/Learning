7857cbd23a1567eb79d5a93dc50cb06b8f14c9cd
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-07 11:18:07 +0530

    ass

---
+++ SeleniumJava/com.crm.seleniumM4/src/basics/DemoshopFlipkart.java
@@ -0,0 +1,19 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoshopFlipkart {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://demowebshop.tricentis.com/");
+		driver.get("https://www.flipkart.com/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.close(); 
+		
+		
+	}
+
+}
