965424f5dea7cea855c9a481ea0696980b77c9e4
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-07 10:32:43 +0530

    going forward and backward in webpage

---
+++ SeleniumJava/com.crm.seleniumM4/src/basics/MoveForwardBackward.py
@@ -0,0 +1,16 @@
+from selenium import webdriver; 
+import time
+
+driver = webdriver.Chrome(); 
+driver.maximize_window();
+driver.get("https://demowebshop.tricentis.com/")
+time.sleep(3); 
+driver.get("https://google.com")
+time.sleep(3); 
+driver.back()
+time.sleep(3); 
+driver.forward();
+time.sleep(3); 
+driver.refresh();
+time.sleep(5); 
+driver.close();
