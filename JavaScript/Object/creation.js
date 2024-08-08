a339e7aafc8d32073487b39ab714ed34757bcad1
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-08 14:25:27 +0530

    person object

--- JavaScript/Object/creation.js
+++ JavaScript/Object/creation.js
@@ -6,3 +6,15 @@ let car = {
 };  
         
 console.log(`Car company is ${car.brand} and model name is ${car.model} and car price is ${car.price}`);
+
+let person = {
+        name : "Kunal", 
+        add : "Pune" , 
+        designameion: null, 
+        gf : undefined , 
+        driving : () => {
+                console.log("Kunal lovs undefined")
+        }
+}; 
+
+console.log(person)
