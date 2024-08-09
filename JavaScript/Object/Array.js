11d26c02de461942668dbec2f7f9ee49b91a39f0
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-09 15:22:34 +0530

    Create Array.js

---
+++ JavaScript/Object/Array.js
@@ -0,0 +1,8 @@
+let a = [1,"asf", null, true]
+console.log(a)
+
+let arr = [10, 20, "hello", ()=>{console.log("executed")}, {name:"tinku"}, [100,200,300] ];
+arr[3](); 
+
+let b = new Array(10, 20, 30, 40, "haha")
+console.log(b)
