4bc326284b54c1cf022a5c0b34b00294354b993c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-09 10:01:30 +0530

    added code

--- Java/javaReference/Chapter15/src/lambdaExpressionsAndExceptions/DoubleNumericArrayFunc.java
+++ Java/javaReference/Chapter15/src/lambdaExpressionsAndExceptions/DoubleNumericArrayFunc.java
@@ -1,8 +1,40 @@
 package lambdaExpressionsAndExceptions;
 
+// Throw an exception from a lambda expression
+
+interface DoubleNumericArrayFunc {
+	double func(double[] n) throws EmptyArrayException; 
+} 
+
+class EmptyArrayException extends Exception {
+	EmptyArrayException() {
+		super("Array Empty");
+	}
+}
 
 public class DoubleNumericArrayFunc {
 	
-	
+	public static void main(String[] args) throws EmptyArrayException{
+		double[] values = {1.0, 2.0, 3.0, 4.0}; 
+
+		// This block lambda computes the average of an array of doubles. 
+
+		DoubleNumericArrayFunc average = (n) -> {
+			double sum = 0; 
+			if(n.length == 0)
+				throw new EmptyArrayException(); 
+
+			for (int i = 0; i < n.length ; i++) 
+				sum += n[i]; 
+
+			reutn sum / n.length; 
+		};
+
+		// This works file
+		System.out.println("The average is : " + average.func(values));
+
+		//this would throw an exception  
+		System.out.println("The average is : " + average.func(new double[0]));
+	}
 
 }
