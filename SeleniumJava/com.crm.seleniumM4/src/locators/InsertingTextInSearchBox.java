cfab5614c5d20f6df640c4deeb5bc839c656ce60
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-09 09:14:29 +0530

    Create InsertingTextInSearchBox.java

---
+++ SeleniumJava/com.crm.seleniumM4/src/locators/InsertingTextInSearchBox.java
@@ -0,0 +1,37 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class InsertingTextInSearchBox {
+	public static void main(String[] args) throws InterruptedException {
+		String expected_result = "https://demowebshop.tricentis.com/";
+		ChromeDriver driver = new ChromeDriver();
+		//maximize the browser. 
+		driver.manage().window().maximize(); 
+		// Waiting condition 
+		Thread.sleep(3000);
+		// enter into dws. 
+		driver.get(expected_result);
+		String actual_result = driver.getCurrentUrl(); 
+		if(expected_result.equals(actual_result)) {
+			System.out.println("I'm in dws page");
+			WebElement search_field = driver.findElement(By.tagName("input"));
+			search_field.sendKeys("iphone");
+			driver.findElement(By.className("button-1 search-box-button")).click();
+			Thread.sleep(5000);	
+			System.out.println("successfully sent iphone into searchfiled");
+			
+		}
+		else {
+			System.out.println("I'm not in dws page");
+			driver.close();
+		
+		}
+		
+		driver.close();
+	}
+
+}
