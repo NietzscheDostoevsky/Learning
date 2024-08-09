f298f6cdbbd5f30bf36fc813b0ecb64f7dc4f6d4
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-09 09:01:33 +0530

    shopping cart using classname

---
+++ SeleniumJava/com.crm.seleniumM4/src/locators/ShoppingCartUsingClassName.java
@@ -0,0 +1,31 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ShoppingCartUsingClassName {
+	public static void main(String[] args) throws InterruptedException {
+			
+			ChromeDriver driver = new ChromeDriver(); 
+			driver.manage().window().maximize();		
+			String url = "https://demowebshop.tricentis.com/";
+			driver.get(url);
+			
+			String fetched_url = driver.getCurrentUrl();
+			if(url.equals(fetched_url)) {
+				System.out.println("I am on DWS Website");
+				
+				Thread.sleep(2000);
+				// going to register page by clicking on register
+				driver.findElement(By.className("cart-label")).click();
+				Thread.sleep(5000);
+				
+			}
+			else {
+				System.out.println("I am not on dws websiite");
+				driver.close();
+			}
+			
+			driver.close();
+	}
+}
