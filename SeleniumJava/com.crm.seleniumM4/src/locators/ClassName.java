ec93e50b77209266427abef467e4285fca22b209
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-09 08:44:11 +0530

    renamed

--- SeleniumJava/com.crm.seleniumM4/src/locators/ClassName.java
+++ SeleniumJava/com.crm.seleniumM4/src/locators/ClassName.java
@@ -3,7 +3,7 @@ package locators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DisadvantageOfTagname {
+public class ClassName {
 
 	public static void main(String[] args) throws InterruptedException {
 		
