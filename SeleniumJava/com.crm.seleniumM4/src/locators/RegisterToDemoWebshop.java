63a00fea05a0f74b0e5e0e0d1ee72e741b291695
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-10 09:35:02 +0530

    register to demo web shop

---
+++ SeleniumJava/com.crm.seleniumM4/src/locators/RegisterToDemoWebshop.java
@@ -0,0 +1,60 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class RegisterToDemoWebshop {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeDriver driver = new ChromeDriver(); 
+		driver.manage().window().maximize();
+		
+		String demoWebshopURL = "https://demowebshop.tricentis.com/";
+		driver.get(demoWebshopURL);
+		String webShopTitle = "Demo Web Shop";
+		
+		// verification and continue 
+		if(driver.getTitle().equals(webShopTitle)){
+			System.out.println("Demo Web Shop Opened");
+			Thread.sleep(2000);
+			
+			// Clicking on register page. 
+			driver.findElement(By.linkText("Register")).click();
+			
+			String regURL = "https://demowebshop.tricentis.com/register";
+			if(driver.getCurrentUrl().equals(regURL)) {
+				System.out.println("On register Page");
+				
+				Thread.sleep(2000);
+				
+				// Filling registration details. 
+				driver.findElement(By.id("gender-male")).click();
+				driver.findElement(By.id("FirstName")).sendKeys("RandomFirstName");
+				driver.findElement(By.id("LastName")).sendKeys("RandomLastName");
+				driver.findElement(By.id("Email")).sendKeys("abc@xyz.com");
+				driver.findElement(By.id("Password")).sendKeys("secretPass");
+				driver.findElement(By.id("ConfirmPassword")).sendKeys("secretPass");
+				
+				// wait and click register button 
+				Thread.sleep(5000);
+				driver.findElement(By.id("register-button")).click();
+				
+				
+			} else {
+				System.out.println("check URL");
+			}
+			
+			
+		}
+		else {
+			System.out.println("Change your URL");
+			
+		}
+		Thread.sleep(5000);
+		driver.close();
+
+	}
+
+}
