09e36c92ced5e66afd3ff313fedf6bc8b2c15ac6
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-10 15:49:20 +0530

    for each

--- JavaScript/Object/Array.js
+++ JavaScript/Object/Array.js
@@ -12,9 +12,18 @@ console.log(arr[5][2])
 //! Array methods 
 // ! PUsh pop  shift unshift.   
 
-let numarr = [63, 847, 555, 17, 555, 97, 555, 903, 75]
+let numarr = [63, 847, 555, 17, 555, 97, 555, 903, 75];
 
-for (let i = 0; i < numarr.length; i++) {
-    if(numarr[i] == 555)
-        console.log(i)
+// for (let i = 0; i < numarr.length; i++) {
+//     if(numarr[i] == 555)
+//         console.log(i)
+// }
+
+// numarr.forEach((val, ind, arr)=>{
+//     console.log(val, ind, arr)
+// })
+
+for (val in numarr) {
+    console.log(val)
 }
+for (val of numarr) console.log(val)
