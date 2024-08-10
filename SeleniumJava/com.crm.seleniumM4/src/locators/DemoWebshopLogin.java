019f79de9fa427f5b7b285dd8f4f4770786f8164
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-10 10:10:02 +0530

    refactored code

--- SeleniumJava/com.crm.seleniumM4/src/locators/DemoWebshopLogin.java
+++ SeleniumJava/com.crm.seleniumM4/src/locators/DemoWebshopLogin.java
@@ -6,35 +6,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DemoWebshopLogin {
 
 	public static void main(String[] args) throws InterruptedException {
-		
-		ChromeDriver driver = new ChromeDriver(); 
+
+		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		String demoWebshopURL = "https://demowebshop.tricentis.com/";
 		driver.get(demoWebshopURL);
-		
-		if(driver.getCurrentUrl().equals(demoWebshopURL)) {
-			
+
+		if (driver.getCurrentUrl().equals(demoWebshopURL)) {
+
 			System.out.println("Demo Web Shop Opened");
 			Thread.sleep(2000);
-			
-			
-			// go to login page. 
+
+			// go to login page.
 			driver.findElement(By.linkText("Log in")).click();
 			String loginURL = "https://demowebshop.tricentis.com/login";
-			
-			if(driver.getCurrentUrl().equals(loginURL)) {
+
+			if (driver.getCurrentUrl().equals(loginURL)) {
 				System.out.println("On login page");
-				Thread.sleep(3000);
-				
-				// Fill login details. 
-				driver.findElement(By.id("Email")).sendKeys("FakeLogin");
-				driver.findElement(By.id("Password")).sendKeys("FakePass");
-				driver.findElement(By.id("RememberMe")).click();
-				
-				//Wait and click
-				Thread.sleep(3000); 
-				driver.findElement(By.className("login-button")).click();
-				Thread.sleep(5000);				
+				fillLoginPage(driver);
 			} else {
 				System.out.println("Login page not opened");
 			}
@@ -48,6 +37,20 @@ public class DemoWebshopLogin {
 		Thread.sleep(5000);
 		driver.close();
 		
+	}
+	
+	public static void fillLoginPage(ChromeDriver driver) throws InterruptedException {
+		Thread.sleep(3000);
+		
+		// Fill login details. 
+		driver.findElement(By.id("Email")).sendKeys("FakeLogin");
+		driver.findElement(By.id("Password")).sendKeys("FakePass");
+		driver.findElement(By.id("RememberMe")).click();
+		
+		//Wait and click
+		Thread.sleep(3000); 
+		driver.findElement(By.className("login-button")).click();
+		Thread.sleep(5000);	
 	}
 
 }
--- SeleniumJava/com.crm.seleniumM4/src/locators/RegisterToDemoWebshop.java
+++ SeleniumJava/com.crm.seleniumM4/src/locators/RegisterToDemoWebshop.java
@@ -3,59 +3,54 @@ package locators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class RegisterToDemoWebshop {
 
 	public static void main(String[] args) throws InterruptedException {
-		
-		ChromeDriver driver = new ChromeDriver(); 
+
+		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
+
 		String demoWebshopURL = "https://demowebshop.tricentis.com/";
 		driver.get(demoWebshopURL);
 		String webShopTitle = "Demo Web Shop";
-		
-		// verification and continue 
-		if(driver.getTitle().equals(webShopTitle)){
+
+		// verification and continue
+		if (driver.getTitle().equals(webShopTitle)) {
 			System.out.println("Demo Web Shop Opened");
 			Thread.sleep(2000);
-			
-			// Clicking on register page. 
+
+			// Clicking on register page.
 			driver.findElement(By.linkText("Register")).click();
-			
+
 			String regURL = "https://demowebshop.tricentis.com/register";
-			if(driver.getCurrentUrl().equals(regURL)) {
+			if (driver.getCurrentUrl().equals(regURL)) {
 				System.out.println("On register Page");
-				
 				Thread.sleep(2000);
-				
-				// Filling registration details. 
-				driver.findElement(By.id("gender-male")).click();
-				driver.findElement(By.id("FirstName")).sendKeys("RandomFirstName");
-				driver.findElement(By.id("LastName")).sendKeys("RandomLastName");
-				driver.findElement(By.id("Email")).sendKeys("abc@xyz.com");
-				driver.findElement(By.id("Password")).sendKeys("secretPass");
-				driver.findElement(By.id("ConfirmPassword")).sendKeys("secretPass");
-				
-				// wait and click register button 
-				Thread.sleep(5000);
-				driver.findElement(By.id("register-button")).click();
-				
-				
+				fillRegistration(driver);
 			} else {
 				System.out.println("check URL");
 				driver.close();
 			}
-			
-			
-		}
-		else {
-			System.out.println("Change your URL");
-			
-		}
-		Thread.sleep(5000);
-		driver.close();
 
+		} else {
+			System.out.println("Change your URL");
+		}
+		Thread.sleep(3000);
+		driver.close();
+	}
+
+	public static void fillRegistration(ChromeDriver driver) throws InterruptedException {
+		// Filling registration details.
+		driver.findElement(By.id("gender-male")).click();
+		driver.findElement(By.id("FirstName")).sendKeys("RandomFirstName");
+		driver.findElement(By.id("LastName")).sendKeys("RandomLastName");
+		driver.findElement(By.id("Email")).sendKeys("abc@xyz.com");
+		driver.findElement(By.id("Password")).sendKeys("secretPass");
+		driver.findElement(By.id("ConfirmPassword")).sendKeys("secretPass");
+
+		// wait and click register button
+		Thread.sleep(2000);
+		driver.findElement(By.id("register-button")).click();
 	}
 
 }
