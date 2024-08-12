4fcd07b6f12e90e171df89a81aeb8f620e848464
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-12 18:31:29 +0530

    Update Array.js

--- JavaScript/Object/Array.js
+++ JavaScript/Object/Array.js
@@ -31,3 +31,78 @@ for (val of numarr) console.log(val)
 numarr.map((val, indx, arr) => {
     console.log(val, indx, arr)
 })
+
+console.log("_____________________________")
+let nums = [190, 380, 220, 301, 870, 667, 7]
+let newnums = nums.sort();
+console.log(newnums)
+
+let sortnums = numarr.sort((a,b)=> {
+    return a-b;
+})
+console.log(sortnums)
+
+let newArr = [];
+//let numarr = [5, 2, 9, 1, 5, 6]; // Example array to sort
+
+for (let i = 0; i < numarr.length; i++) {
+    for (let j = 0; j < numarr.length - i - 1; j++) {
+        if (numarr[j] > numarr[j + 1]) {
+            // Swap the elements
+            let temp = numarr[j];
+            numarr[j] = numarr[j + 1];
+            numarr[j + 1] = temp;
+        }
+    }
+}
+
+console.log(numarr); // Output the sorted array
+
+
+let c = numarr.filter((val, i)=> {
+    return val!==555;
+})
+console.log(c)
+
+let users = [
+    {
+        name: "tinku",
+        id: 1
+    } , 
+    {
+        name: "pinku",
+        id: 2
+    } , 
+    {
+        name: "rinku",
+        id: 3
+    } , 
+    {
+        name: "kinku",
+        id: 4
+    } , 
+    {
+        name: "rinku",
+        id: 5
+    } 
+]
+
+let updatedArr = users.filter((val) => {
+    return val.id !== 2;
+})
+
+console.log(updatedArr);
+
+console.log(nums.find((val, i) => {
+    if(val == 380)
+        return i;
+}));
+
+// find () will not return index, only the element. 
+
+let arr3 = [10, 20, 30, 40, 20] 
+let a2 = arr3.reduce((sum, val) => {
+    return sum += val;
+});
+console.log("-----------------")
+console.log(a2);
