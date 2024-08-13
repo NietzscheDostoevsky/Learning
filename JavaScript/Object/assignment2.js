a4ef0e7828f17b08027e56a8886d01a129f31051
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-13 10:07:31 +0530

    2

--- JavaScript/Object/assignment2.js
+++ JavaScript/Object/assignment2.js
@@ -9,3 +9,18 @@ a1Fruit = ['mango', 'banana', 'pineapple', 'blueberry', 'papaya']
 a1Fruit.forEach((element, index) => {
     console.log("I like " + element )
 })
+
+console.log("---------------------")
+
+//2.  
+a2color = ['red', 'blue', 'black', 'white', 'green']
+for ( index in a2color) {
+    console.log(`Index: ${index} , Color: ${a2color[index]}`)
+}
+
+a2 = [2, 4, 6, 8, 10]; 
+for (index in a2) {
+    let element = a2[index];
+    a2[index] = element * 2; 
+}
+console.log(a2)
