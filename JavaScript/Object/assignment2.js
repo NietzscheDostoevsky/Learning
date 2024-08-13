f0c39b04d009bafcc4aaae1d6f7e165eab4cc0a7
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-13 09:58:13 +0530

    q1

---
+++ JavaScript/Object/assignment2.js
@@ -0,0 +1,11 @@
+// Ques 1
+
+a1 = [2, 4, 6, 8, 10]; 
+a1.forEach((element, index) => {
+    console.log(element, index)
+});
+
+a1Fruit = ['mango', 'banana', 'pineapple', 'blueberry', 'papaya']
+a1Fruit.forEach((element, index) => {
+    console.log("I like " + element )
+})
