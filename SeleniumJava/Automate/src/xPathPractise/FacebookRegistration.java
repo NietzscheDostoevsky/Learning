93f3226daa32d271cb455ebd277e3b3448d193f4
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-14 20:41:31 +0530

    Update FacebookRegistration.java

--- SeleniumJava/Automate/src/xPathPractise/FacebookRegistration.java
+++ SeleniumJava/Automate/src/xPathPractise/FacebookRegistration.java
@@ -1,4 +1,10 @@
 package xPathPractise;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FacebookRegistration {
+
+    public static void main(String[] args) {
+        ChromeDriver driver = new ChromeDriver();
+    }
 }
