18b182a28dbcfce0e04f4a0c0df88362253e4df6
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-14 09:05:52 +0530

    registered using css locator

--- SeleniumJava/com.crm.seleniumM4/src/locators/DWSLoginCSSSelector.java
+++ SeleniumJava/com.crm.seleniumM4/src/locators/DWSLoginCSSSelector.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DWSLoginCSSSelector {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		
 		ChromeDriver driver = new ChromeDriver(); 
 		driver.manage().window().maximize();
