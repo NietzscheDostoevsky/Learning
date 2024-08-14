34ffff2501a4d4dd4422126881c8d815f17a29f0
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-14 20:20:04 +0530

    Create FacebookRegistrationXPath.java

---
+++ SeleniumPython/com.selenium.practice/src/xPath/FacebookRegistrationXPath.java
@@ -0,0 +1,27 @@
+package xPath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+ * Assignment :  
+Write a script for facebook  
+Open the browser.  
+Maximize the browser.  
+Enter into the fb landing page.  
+Verify the landing page.  
+Click the create new account button.  
+Fill all the details, except dropdown.  
+Use any locators.  
+ */
+
+public class FacebookRegistrationXPath {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver(); 
+		
+
+	}
+
+}
