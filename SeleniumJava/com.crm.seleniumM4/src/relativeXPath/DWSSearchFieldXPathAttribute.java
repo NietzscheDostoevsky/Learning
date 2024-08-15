b1c4dcaff3b32c52bd6cf57f8af34dea673a2d37
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-15 09:22:12 +0530

    using relative xpath and attribute

---
+++ SeleniumJava/com.crm.seleniumM4/src/relativeXPath/DWSSearchFieldXPathAttribute.java
@@ -0,0 +1,40 @@
+package relativeXPath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class DWSSearchFieldXPathAttribute {
+	public static void main(String[] args) throws InterruptedException {
+		String expected_result = "https://demowebshop.tricentis.com/";
+		ChromeDriver driver = new ChromeDriver();
+		//maximize the browser. 
+		driver.manage().window().maximize(); 
+		// Waiting condition 
+		Thread.sleep(3000);
+		// enter into dws. 
+		driver.get(expected_result);
+		String actual_result = driver.getCurrentUrl(); 
+		if(expected_result.equals(actual_result)) {
+			System.out.println("I'm in dws page");
+			WebElement search_field = driver.findElement(By.xpath("//input[@value='Search store']"));
+			search_field.sendKeys("iphone");
+			Thread.sleep(3000);
+			search_field.sendKeys(Keys.RETURN);
+			
+			Thread.sleep(5000);	
+			System.out.println("successfully sent iphone into searchfiled");
+			
+		}
+		else {
+			System.out.println("I'm not in dws page");
+			driver.close();
+		
+		}
+		
+		driver.close();
+	}
+
+}
