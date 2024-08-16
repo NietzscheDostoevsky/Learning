86f3d11443016db9a6ddf5dd197b731cfe860fa5
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-16 17:26:01 +0530

    Create WrapperExample.java

---
+++ Java/QSP/Strings/Strings/src/WrapperExample.java
@@ -0,0 +1,16 @@
+public class WrapperExample {
+    public static void main(String[] args) {
+
+       String a = "12";
+       Integer b = Integer.parseInt(a);
+       System.out.println(b);
+
+       byte c = Byte.parseByte(a);
+        System.out.println(c);
+
+        String d = "12a";
+        Integer e = Integer.parseInt(d);
+        System.out.println(e);
+
+    }
+}
