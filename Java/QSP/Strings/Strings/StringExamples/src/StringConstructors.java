3c2ea9f02010c07844d0a43748bac94dd52845a4
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-17 19:10:04 +0530

    Create StringConstructors.java

---
+++ Java/QSP/Strings/Strings/StringExamples/src/StringConstructors.java
@@ -0,0 +1,8 @@
+public class StringConstructors {
+    public static void main(String[] args) {
+
+
+        char [] arr = {'a', 'b', 'c', 'd'};
+        System.out.println(new String(arr));
+    }
+}
