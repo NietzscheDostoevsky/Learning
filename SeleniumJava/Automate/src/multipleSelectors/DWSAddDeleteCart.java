02d4db9dd568a25aa703103608f3623aa8401dd1
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 11:38:19 +0530

    added questionare

--- SeleniumJava/Automate/src/multipleSelectors/DWSAddDeleteCart.java
+++ SeleniumJava/Automate/src/multipleSelectors/DWSAddDeleteCart.java
@@ -1,4 +1,17 @@
 package multipleSelectors;
 
+/**
+ * Write a script for demo webshop.
+ *      Open the browser.
+ *      Maximize the browser.
+ *      Enter into DWS.
+ *      Verify the page using title.
+ *      Click the digital download menu bar.
+ *      Add all 3 mentioned products to cart, by using find elements.
+ *      Remove all three products from the shopping cart.
+ *      Click checkbox
+ *      Click update shopping cart.
+ */
+
 public class DWSAddDeleteCart {
 }
--- SeleniumJava/Automate/src/multipleSelectors/DWSGiftCard.java
+++ SeleniumJava/Automate/src/multipleSelectors/DWSGiftCard.java
@@ -1,4 +1,19 @@
 package multipleSelectors;
 
+/**
+ * Write the script for demo web shop
+ *      Open the browser.
+ *      Maximize the browser.
+ *      Enter into DWS.
+ *      Verify the page by using URL.
+ *      After verification, click the gift card section.
+ *      Add the product which is having $5.
+ *      Fill all the details on that page.
+ *      Change the quantity to 3
+ *      Click add to cart.
+ *      Check if the product is added successfully or not.
+ *      Close the browser.
+ */
+
 public class DWSGiftCard {
 }
