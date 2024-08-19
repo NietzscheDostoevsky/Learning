acbcb6d07bbc640baf113c59f738c01325bad178
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 15:27:03 +0530

    done

--- SeleniumJava/Automate/src/multipleSelectors/DWSAddDeleteCart.java
+++ SeleniumJava/Automate/src/multipleSelectors/DWSAddDeleteCart.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Driver;
 import java.util.List;
 
 /**
@@ -14,9 +15,9 @@ import java.util.List;
  *      ✅ Verify the page using title.
  *      ✅ Click the digital download menu bar.
  *      ✅ Add all 3 mentioned products to cart, by using find elements.
- *      Remove all three products from the shopping cart.
- *      Click checkbox
- *      Click update shopping cart.
+ *      ✅ Remove all three products from the shopping cart.
+ *      ✅ Click checkbox
+ *      ✅Click update shopping cart.
  */
 
 public class DWSAddDeleteCart {
@@ -31,23 +32,43 @@ public class DWSAddDeleteCart {
         if (driver.getTitle().equals(expectedTitle)) {
             System.out.println("Page verified by title ✅");
             System.out.println("I am on DWS homepage");
-
+            Thread.sleep(3000);
             // click on digital download menu bar
             driver
                     .findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/ul[1]/li[5]/a"))
                     .click();
             System.out.println("Digital downloads ✅ ");
 
+            Thread.sleep(2000);
             // finding and adding all 3 mentioned products to cart
             List<WebElement> items = driver.findElements(By.xpath("//div[@class='item-box']"));
             for (WebElement item : items) {
                 item.findElement(By.tagName("input")).click();
+                Thread.sleep(1500);
+            }
+            System.out.println("Added all 3 times to cart ✅");
+
+            // remove all three items from cart
+
+            //navigate to shopping cart webpage
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//*[@id='topcartlink']/a/span[1]")).click();
+            Thread.sleep(3000);
+            //finding and clicking remove checkboxes
+            List<WebElement> cartRemove = driver.findElements(By.className("remove-from-cart"));
+            System.out.println("Total items in cart to remove: " + cartRemove.size());
+            // clicking the remove checkbox
+            for (WebElement checkbox: cartRemove) {
+                checkbox.findElement(By.tagName("input")).click();
                 Thread.sleep(1000);
             }
-            System.out.println("Add all 3 times to cart ✅");
+            System.out.println("Clicked on all remove checkboxes ✅");
 
-
-
+            // clicking on update shopping cart
+            driver.findElement(By.name("updatecart")).click();
+            System.out.println("Shopping cart updated ✅");
+            Thread.sleep(5000);
+            driver.close();
 
         } else {
             System.out.println("Not on DWS page");
