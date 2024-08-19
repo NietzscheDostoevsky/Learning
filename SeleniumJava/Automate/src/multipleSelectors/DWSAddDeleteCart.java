7033ad8ec7aee978966233697a56a421d0a19378
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 15:01:25 +0530

    added items to cart

--- SeleniumJava/Automate/src/multipleSelectors/DWSAddDeleteCart.java
+++ SeleniumJava/Automate/src/multipleSelectors/DWSAddDeleteCart.java
@@ -1,17 +1,60 @@
 package multipleSelectors;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 /**
  * Write a script for demo webshop.
- *      Open the browser.
- *      Maximize the browser.
- *      Enter into DWS.
- *      Verify the page using title.
- *      Click the digital download menu bar.
- *      Add all 3 mentioned products to cart, by using find elements.
+ *      ✅ Open the browser.
+ *      ✅ Maximize the browser.
+ *      ✅ Enter into DWS.
+ *      ✅ Verify the page using title.
+ *      ✅ Click the digital download menu bar.
+ *      ✅ Add all 3 mentioned products to cart, by using find elements.
  *      Remove all three products from the shopping cart.
  *      Click checkbox
  *      Click update shopping cart.
  */
 
 public class DWSAddDeleteCart {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        String dwsURL = "https://demowebshop.tricentis.com/";
+        driver.get(dwsURL);
+
+        String expectedTitle = "Demo Web Shop";
+
+        if (driver.getTitle().equals(expectedTitle)) {
+            System.out.println("Page verified by title ✅");
+            System.out.println("I am on DWS homepage");
+
+            // click on digital download menu bar
+            driver
+                    .findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/ul[1]/li[5]/a"))
+                    .click();
+            System.out.println("Digital downloads ✅ ");
+
+            // finding and adding all 3 mentioned products to cart
+            List<WebElement> items = driver.findElements(By.xpath("//div[@class='item-box']"));
+            for (WebElement item : items) {
+                item.findElement(By.tagName("input")).click();
+                Thread.sleep(1000);
+            }
+            System.out.println("Add all 3 times to cart ✅");
+
+
+
+
+        } else {
+            System.out.println("Not on DWS page");
+            System.out.println("Check your URL");
+            Thread.sleep(3000);
+            driver.close();
+        }
+
+    }
 }
