c261c2922bb0d9dcd8a66294cd279ed64c66da3c
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-20 18:20:42 +0530

    to upper case and toLowerCAse implementation

--- Java/QSP/Strings/Strings/StringExamples/src/MyString.java
+++ Java/QSP/Strings/Strings/StringExamples/src/MyString.java
@@ -1,7 +1,11 @@
-final class MyString {
+public final class MyString {
     String str = null;
     MyString(String str) {
         this.str = str;
+    }
+    MyString() {
+        // no argument constructor
+        str = "";
     }
 
     public int length() {
@@ -17,9 +21,56 @@ final class MyString {
         return count;
     }
 
+    public boolean isEmpty() {
+        if(str==null)
+            throw new NullPointerException("String object not created.");
+        if(str.length()==0)
+            return true;
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return str;
+    }
+
+    public String toUpperCase() {
+        String op = "";
+        for (int i = 0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch >=97 && ch<=122)
+                op += (char)(ch-32);
+            else
+                op +=ch;
+        }
+        return op;
+    }
+
+    public String toLowerCase() {
+        String op = "";
+        for (int i = 0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch >=65 && ch<=90)
+                op += (char)(ch+32);
+            else
+                op +=ch;
+        }
+        return op;
+    }
+
+
+
     public static void main(String[] args) {
         MyString mystr = new MyString("Saurabh");
         System.out.println(mystr.length());
+        System.out.println(mystr.isEmpty());
+
+        MyString str2 = new MyString();
+        System.out.println(str2.isEmpty());
+        System.out.println(mystr.toUpperCase());
+        System.out.println(new MyString("HahaHehe").toUpperCase());
+        System.out.println(new MyString("HahaHehe").toLowerCase());
+
     }
 }
 
