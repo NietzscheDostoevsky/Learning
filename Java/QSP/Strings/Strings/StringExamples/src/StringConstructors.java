ee69ce834b69a3ed52601a825ee89d954d1abcbb
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-20 17:45:25 +0530

    Update StringConstructors.java

--- Java/QSP/Strings/Strings/StringExamples/src/StringConstructors.java
+++ Java/QSP/Strings/Strings/StringExamples/src/StringConstructors.java
@@ -2,7 +2,21 @@ public class StringConstructors {
     public static void main(String[] args) {
 
 
-        char [] arr = {'a', 'b', 'c', 'd'};
-        System.out.println(new String(arr));
+//        char [] arr = {'a', 'b', 'c', 'd'};
+//        System.out.println(new String(arr));
+//
+//        String str1= "RAMESH";
+//        StringBuffer sb = new StringBuffer(str1);
+//        System.out.println(sb);
+//        sb.reverse();
+//        System.out.println(sb);
+//        String str = new String(sb);
+//        System.out.println(str.contentEquals(sb)?"Palindrome":"Not palindrome");
+
+        String name = "saurabh";
+        System.out.println(name.codePointAt(5));
+        System.out.println(name.codePointBefore(6));
+        System.out.println(name.codePointCount(5,7));
+
     }
 }
