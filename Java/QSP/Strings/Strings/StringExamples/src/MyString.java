1d6ef1ac243912398256f59374dfd36bf946b28f
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-20 17:44:41 +0530

    myString class

---
+++ Java/QSP/Strings/Strings/StringExamples/src/MyString.java
@@ -0,0 +1,26 @@
+public final class MyString {
+    String str = null;
+    MyString(String str) {
+        this.str = str;
+    }
+
+    public int length() {
+        int count = 0;
+        for (int i = 0;true;i++) {
+            try{
+                str.charAt(i);
+                count++;
+            } catch (StringIndexOutOfBoundsException e) {
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        MyString mystr = new MyString("Saurabh");
+        System.out.println(mystr.length());
+    }
+}
+
+
