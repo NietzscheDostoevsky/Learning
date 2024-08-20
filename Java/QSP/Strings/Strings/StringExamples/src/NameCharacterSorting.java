e0faf0940cb7ca4032bb6beea671a666ffca57e1
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-20 17:45:29 +0530

    Update NameCharacterSorting.java

--- Java/QSP/Strings/Strings/StringExamples/src/NameCharacterSorting.java
+++ Java/QSP/Strings/Strings/StringExamples/src/NameCharacterSorting.java
@@ -1,23 +1,23 @@
-import java.util.Scanner;
-
-public class NameCharacterSorting {
-    public static void main(String[] args) {
-        String name = "Saurabh";
-
-    }
-
-    public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
-        int n = arr.length;
-        boolean swapped;
-
-        for(int i = 0; i < n - 1; i++) {
-            if (arr[j].compareTo(arr[j + 1]) > 0) {
-                T temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = true;
-            }
-        }
-        if(!swapped) break;\
-    }
-}
+//import java.util.Scanner;
+//
+//public class NameCharacterSorting {
+//    public static void main(String[] args) {
+//        String name = "Saurabh";
+//
+//    }
+//
+//    public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
+//        int n = arr.length;
+//        boolean swapped;
+//
+//        for(int i = 0; i < n - 1; i++) {
+//            if (arr[j].compareTo(arr[j + 1]) > 0) {
+//                T temp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = temp;
+//                swapped = true;
+//            }
+//        }
+//        if(!swapped) break;
+//    }
+//}
