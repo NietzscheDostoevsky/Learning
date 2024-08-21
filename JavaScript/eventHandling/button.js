878c604a4eddc6e1f449939aad7de4d40484be76
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-21 18:15:31 +0530

    heart button

---
+++ JavaScript/eventHandling/button.js
@@ -0,0 +1,22 @@
+// let main = document.querySelector('main')
+// let divs = document.querySelectorAll('div') 
+
+// let singleclick = () => {
+//     console.log(divs);
+
+// }
+
+// let btn = document.querySelector('button');
+// let func = () => {
+//     console.log("button clicked");
+// }
+
+// btn.addEventListener("click", ()=> {
+//     console.log("button clicked using addEventLister()")
+// })
+
+// btn.onclick = func;
+
+let func = () => {
+    console.log("button clicked from HTML")
+};
