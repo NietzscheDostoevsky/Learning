a90a278eec4141294e60aa26d82c816507582be5
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-21 08:32:44 +0530

    data pass verification

--- SeleniumJava/com.crm.seleniumM4/src/webElementMethods/getAttributeMethods.java
+++ SeleniumJava/com.crm.seleniumM4/src/webElementMethods/getAttributeMethods.java
@@ -17,6 +17,14 @@ public class getAttributeMethods {
 	    WebElement searchField = driver.findElement(By.name("q"));
 	    String attributeValue = searchField.getAttribute("id");
 	    System.out.println(attributeValue);
+	    
+	    String data = "iPhone";
+	    searchField.sendKeys(data);
+	    if (searchField.getAttribute("value").equals(data))
+	    	System.out.println("Succesfully passed the data");
+	    else 
+	    	System.out.println("data passing unsuccessfull");
+	    
 	    driver.close();
 	}
 
