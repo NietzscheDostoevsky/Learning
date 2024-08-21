3b638c85717e245889cf0228a84446d6f1eb5af1
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-21 18:15:21 +0530

    demo

--- JavaScript/dom/demo.js
+++ JavaScript/dom/demo.js
@@ -10,13 +10,24 @@
 // let element = document.querySelector('#firstDiv')
 // console.log(element)
 
-let elements = document.querySelector('#firstele')
-console.log(elements.style.background)
-elements.style.color = 'yellow'
-elements.style.height = '50px'
+// let elements = document.querySelector('#firstele')
+// console.log(elements.style.background)
+// elements.style.color = 'yellow'
+// elements.style.height = '50px'
 
-let para = document.createElement('p');
-console.log(para)
-para.innerText = "I am paragraph"
+// let para = document.createElement('p');
+// console.log(para)
+// para.innerText = "I am paragraph"
 
-elements.append(para)
+// elements.append(para)
+
+let btn = document.querySelector('button')
+console.log(btn);
+var i = 0;
+
+let func = () => {
+    console.log("You clicked a button" + i++)
+}
+btn.onclick = func;
+
+console.log(btn)
