4682bf4946586ddd7bdab44abfb0f8336caf8eba
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-23 10:01:00 +0530

    follow links

---
+++ SeleniumJava/com.crm.seleniumM4/src/usingUtilities/DWSFollowLinks.java
@@ -0,0 +1,30 @@
+package usingUtilities;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utilityClasses.BaseClassDWS;
+
+public class DWSFollowLinks extends BaseClassDWS {
+
+	public static void main(String[] args) {
+		
+		useChrome();
+		
+		List<WebElement> links = driver.findElements(By.xpath("//div[@class='column follow-us']/ul/li/a"));
+		for (WebElement link : links) {
+			
+			String rssLink = "https://demowebshop.tricentis.com/news/rss/1";
+			link.click();
+			System.out.println("clicked");
+			if(rssLink.equals(driver.getCurrentUrl()))
+				driver.navigate().back();		
+			sleep();
+		}
+		
+		postCondition2();
+	}
+
+}
