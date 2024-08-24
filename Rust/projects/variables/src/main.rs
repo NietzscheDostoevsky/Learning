83e2c27cc112f4d69a604042fed4f93588a52082
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-24 21:35:14 +0530

    varibles in rust

--- Rust/projects/variables/src/main.rs
+++ Rust/projects/variables/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 54;
+    println!("The value of x is: {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("{}", THREE_HOURS_IN_SECONDS);
+
+    let y = 6;
+    let y = y + 1;
+
+    println!("The value if y is: {}", y);
+
+    {
+        let y = y * 2;
+        println!("Y inside inner scope is : {}", y);
+    }
+    println!("Y again in outer scope is {}" , y);
 }
