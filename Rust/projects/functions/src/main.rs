4c7439824e9798d2db3e48ef83f774b1cf5510d4
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-25 16:31:36 +0530

    functino declaration

--- Rust/projects/functions/src/main.rs
+++ Rust/projects/functions/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+   println!("Hello, haha");
+
+    another_function(43);
+    print_labeled_measurements(5, '❤');
+}
+
+fn another_function(input: i32) {
+    println!("another functino huey huey input {input}");
+}
+
+fn print_labeled_measurements(num: i32, ch: char) {
+    println!("The measurement is {num}: {ch}");
 }
