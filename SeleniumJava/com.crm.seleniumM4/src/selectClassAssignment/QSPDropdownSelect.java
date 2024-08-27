f7f96107fe35d1d8215e3569f500d2f9c454c7ef
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-27 21:07:43 +0530

    crete

---
+++ SeleniumJava/com.crm.seleniumM4/src/selectClassAssignment/QSPDropdownSelect.java
@@ -0,0 +1,22 @@
+package selectClassAssignment;
+
+/**
+ * Assignment 2 :  QSP demp website 
+ * Open the browser 
+ * Maximize 
+ * Enter into qsp demo site 
+ * Verify using url 
+ * Click UI testing concepts.  
+ * Click web Elements section  
+ * Click dropdown  
+ * Perform all the actions.  
+ */
+
+public class QSPDropdownSelect {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
