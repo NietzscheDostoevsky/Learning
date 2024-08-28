307026f25cba1e6195e796cacdda34cf11379c1d
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-28 21:43:38 +0530

    stale elemente exception

---
+++ SeleniumJava/com.crm.seleniumM4/src/selectClassAssignment/DWSDigitalDownloadsSelect.java
@@ -0,0 +1,48 @@
+package selectClassAssignment;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * Assignment :  
+ * 		Write a script for demo web shop 
+ * 		Open and maximize the browser.  
+ * 		Enter into dws.  
+ * 		Verify the page by using title.  
+ * 		Click the digital downloads.  
+ * 		Handle sort by display view as drop down menu.  
+ * 		Select all the data from first to last.  
+ * 		Close the browser.  
+ */
+
+public class DWSDigitalDownloadsSelect {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver(); 
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		Thread.sleep(3000);
+		if(driver.getTitle().equals("Demo Web Shop")) 
+			System.out.println("Page verified ✔️");
+		driver.findElement(By.xpath("//ul[@class='top-menu']/li[5]")).click();
+		Thread.sleep(3000);
+		Select sort = new Select(driver.findElement(By.id("products-orderby")));
+		List<WebElement> sortOptions = sort.getOptions();
+		
+		for (WebElement ele : sortOptions) {
+			ele.click();
+			Thread.sleep(2000);
+			driver.navigate().back();
+			Thread.sleep(1000);
+		}
+		Thread.sleep(5000);
+		driver.quit();
+		
+	}
+
+}
