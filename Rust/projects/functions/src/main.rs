b303d15d14adb19dc1a93a1d9189cab585ed09ad
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-30 14:28:57 +0530

    function calls

--- Rust/projects/functions/src/main.rs
+++ Rust/projects/functions/src/main.rs
@@ -3,6 +3,9 @@ fn main() {
 
     another_function(43);
     print_labeled_measurements(5, '❤');
+    print!("calling five() : {}\n", five());
+    let x = five();
+    print!("five() : {x}");
 }
 
 fn another_function(input: i32) {
@@ -12,3 +15,8 @@ fn another_function(input: i32) {
 fn print_labeled_measurements(num: i32, ch: char) {
     println!("The measurement is {num}: {ch}");
 }
+
+fn five() -> i32 {
+    5
+}
+
