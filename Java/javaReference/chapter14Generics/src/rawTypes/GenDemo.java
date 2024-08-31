f0185a3978ec0ae5ee592f9d7cee59712dc53703
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-31 12:19:10 +0530

    raw type type safety

--- Java/javaReference/chapter14Generics/src/rawTypes/GenDemo.java
+++ Java/javaReference/chapter14Generics/src/rawTypes/GenDemo.java
@@ -15,10 +15,64 @@ class Gen<Type> {
 	}
 }
 
+// Demostrating raw types. 
 public class GenDemo {
 
 	public static void main(String[] args) {
-
+		
+		//Create a Gen object for Integers. 
+		Gen<Integer> iObj = new Gen<Integer>(88);
+		
+		//Create a Gen object for Strings
+		Gen<String> strObj = new Gen<String>("Testing generics");
+		
+		// creating a Gen object and give it s double value. 
+		Gen raw = new Gen(Double.valueOf(37.73));
+		
+		//Cast here is necessary because type is unknown. 
+		double d = (Double) raw.getObjType();
+		
+		System.out.println("Value of double: " + d);
+		
+		// The use of a raw type can lead to run-time
+		// exceptions. 
+		
+		//int i = (Integer) raw.getObjType(); // RTE ClassCastException
+		
+		//this assignment overrides type safety. 
+		strObj = raw; // No error in this line 
+		//String str = strObj.getObjType();// error CCE.
+		
+		raw = iObj; // again, no error here, but type safety can't be ensured. 
+		d = (Double) raw.getObjType(); // CCE during runtime.  
+		
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
