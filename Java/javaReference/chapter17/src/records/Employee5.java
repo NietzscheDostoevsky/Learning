8eb2a87120e09e4b0cb528498e2413815af132b4
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-02 22:29:08 +0530

    records complete

--- Java/javaReference/chapter17/src/records/Employee5.java
+++ Java/javaReference/chapter17/src/records/Employee5.java
@@ -10,12 +10,17 @@ public record Employee5(String name, int id) {
 		if (i != j) throw new IllegalArgumentException("Multiple commas found");
 		if (i < 1 | name.length() == i+1) throw new IllegalArgumentException("Required format: last, first");
 	}
+	
+	String lastName() {
+		return name.substring(0, name.trim().indexOf(','));
+	}
 }
 
 class Demo2 {
 	public static void main(String[] args) {
 		Employee5[] empList = new Employee5[4];
 		empList[0] = new Employee5("Doe, John", 1047);
+		System.out.println("Last name : " + empList[0].lastName());
 		// No comma between last and first name.
 		empList[1] = new Employee5("Jones Robert", 1048);
 		// Extra commas.
