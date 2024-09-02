d64c632444316f78e1f59a4a2991b0a7ad305c52
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-02 22:14:44 +0530

    data verification in records

---
+++ Java/javaReference/chapter17/src/records/Employee5.java
@@ -0,0 +1,26 @@
+package records;
+
+public record Employee5(String name, int id) {
+
+	public Employee5 {
+		name = name.trim();
+		int i = name.indexOf(',');
+		int j = name.lastIndexOf(',');
+		
+		if (i != j) throw new IllegalArgumentException("Multiple commas found");
+		if (i < 1 | name.length() == i+1) throw new IllegalArgumentException("Required format: last, first");
+	}
+}
+
+class Demo2 {
+	public static void main(String[] args) {
+		Employee5[] empList = new Employee5[4];
+		empList[0] = new Employee5("Doe, John", 1047);
+		// No comma between last and first name.
+		empList[1] = new Employee5("Jones Robert", 1048);
+		// Extra commas.
+		empList[1] = new Employee5("Jones, ,Robert", 1048);
+		// Missing last name.
+		empList[1] = new Employee5(", Robert", 1048);
+	}
+}
