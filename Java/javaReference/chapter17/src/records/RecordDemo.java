2cd6fc1b9a7220dcc9120f014809f4263a291213
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-02 21:18:47 +0530

    records

---
+++ Java/javaReference/chapter17/src/records/RecordDemo.java
@@ -0,0 +1,23 @@
+package records;
+
+record Employee(String name, int id) {};
+
+public class RecordDemo {
+
+	public static void main(String[] args) {
+		
+		Employee[] empList = new Employee[4];
+		
+		empList[0] = new Employee("Doe", 1057);
+		empList[1] = new Employee("Jones, Robert", 1048);
+		empList[2] = new Employee("Smith, Rachel", 1049);
+		empList[3] = new Employee("Martin, Dave", 1050);
+		
+		for (Employee e: empList)
+			System.out.println("The employee ID for " + e.name() + " is " +
+					e.id());
+		
+		System.out.println(empList[0]);
+	}
+
+}
