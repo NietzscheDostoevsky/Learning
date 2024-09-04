2538a0e5d6f2f130cc2aa7872f4635a0371b6766
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-04 10:29:36 +0530

    hover over menu items

--- SeleniumJava/com.crm.seleniumM4/src/actionClassAssignments/MyntraHoverMainMenu.java
+++ SeleniumJava/com.crm.seleniumM4/src/actionClassAssignments/MyntraHoverMainMenu.java
@@ -1,12 +1,28 @@
 package actionClassAssignments;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 import actionsClass.MyntraHomePage;
 
 public class MyntraHoverMainMenu extends MyntraHomePage {
 	public static void main(String[] args) {
 		getMyntraInChrome();
 		
+		WebElement navLinks = driver.findElement(By.className("desktop-navLinks"));
+		List<WebElement> menu = navLinks.findElements(By.className("desktop-navContent"));
 		
+		Actions act = new Actions(driver);
+		for (WebElement link : menu) {
+			act.moveToElement(link).build().perform();
+			sleep(1500);
+		}
+		
+		sleep(3000);
+		driver.quit();
 		
 	}
 }
