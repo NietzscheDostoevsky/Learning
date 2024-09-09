7816898a7fcec0a35d5a02d9b5d121596f8c8567
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-09 10:20:30 +0530

    open in new window

--- SeleniumJava/com.crm.seleniumM4/src/actionsClass/OpenPageInNewWindow.java
+++ SeleniumJava/com.crm.seleniumM4/src/actionsClass/OpenPageInNewWindow.java
@@ -1,8 +1,6 @@
 package actionsClass;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import utilityClasses.BaseClassDWS;
@@ -10,10 +8,16 @@ import utilityClasses.BaseClassDWS;
 public class OpenPageInNewWindow extends BaseClassDWS {
 	public static void main(String[] args) {
 		useChrome();
-		WebElement booksElement = driver.findElement(By.linkText("Books"));
+		//WebElement booksElement = driver.findElement(By.linkText("Books"));
 		Actions actions = new Actions(driver);
-		sleep(3000);
-		actions.keyDown(Keys.SHIFT).click(booksElement).perform();
+//		sleep(3000);
+//		actions.keyDown(Keys.SHIFT).click(booksElement).perform();
+		
+		
+		
+		for (int i = 0; i < 8; i++) {actions.keyDown(Keys.TAB).perform(); sleep(300);}
+		actions.keyUp(Keys.TAB).perform(); sleep(1000);
+		actions.keyDown(Keys.SHIFT).sendKeys(Keys.ENTER).perform();
 		postCondition2();
 		
 	}
