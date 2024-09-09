8efaa7491fa642180ed9c33754533f9f097cc5f8
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-09 12:49:09 +0530

    addec context

--- Java/javaReference/Chapter15/src/methodReferences/InstanceMethWithObjectRefDemo.java
+++ Java/javaReference/Chapter15/src/methodReferences/InstanceMethWithObjectRefDemo.java
@@ -2,30 +2,33 @@ package methodReferences;
 
 // Use an instance method reference with with different objects.
 
+//Situation where you want to specify an instance method that
+	//	can be used with any object of a given class, and 
+	//	not just the specified object. 
+
+
+// The first parameter of the functional interface matches
+// 		the invoking object and the second parameter matches the 
+// 		parameter specified by the method. 
+
 interface MyFunc<T> {
 	boolean func(T v1, T v2);
 }
 
-
+// A class that stores the temperature high for a day.
 class HighTemp {
 	private int hTemp;
+	public HighTemp(int hTemp) { 
+		this.hTemp = hTemp; }
 	
-	public HighTemp(int hTemp) {
-		this.hTemp = hTemp;
-	}
-	
-	boolean sameTemp(HighTemp ht2) {
-		return hTemp == ht2.hTemp;
-	}
-	
-	boolean lessThanTemp (HighTemp ht2) {
-		return hTemp < ht2.hTemp;
-	}	
+	//Instance methods. 
+	boolean sameTemp(HighTemp ht2) { return hTemp == ht2.hTemp; }
+	boolean lessThanTemp (HighTemp ht2) { return hTemp < ht2.hTemp;	}
 }
 
 public class InstanceMethWithObjectRefDemo {
 	
-	static <T> int counter(T[] vals, MyFunc<T> f, T v) {
+	static <T> int counter(T[]vals, MyFunc<T> f, T v) {
 		int count = 0; 
 		for (int i = 0; i < vals.length; i++)
 			if (f.func(vals[i], v)) count++; 
@@ -34,7 +37,8 @@ public class InstanceMethWithObjectRefDemo {
 	
 	public static void main(String[] args) {
 		int count ; 
-		HighTemp[] weekDayHighs = { new HighTemp(89), new HighTemp(82),
+		HighTemp[] weekDayHighs = 
+			  { new HighTemp(89), new HighTemp(82),
 				new HighTemp(90), new HighTemp(89),
 				new HighTemp(89), new HighTemp(91),
 				new HighTemp(84), new HighTemp(83) };
@@ -42,7 +46,8 @@ public class InstanceMethWithObjectRefDemo {
 		count = counter(weekDayHighs, HighTemp::sameTemp, new HighTemp(89));
 		System.out.println(count + " days had a high of 89");
 		
-		HighTemp[] weekDayHighs2 = { new HighTemp(32), new HighTemp(12),
+		HighTemp[] weekDayHighs2 = 
+			  { new HighTemp(32), new HighTemp(12),
 				new HighTemp(24), new HighTemp(19),
 				new HighTemp(18), new HighTemp(12),
 				new HighTemp(-1), new HighTemp(13) };
