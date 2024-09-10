aa48b64815383a48b2a2528183b460a668477682
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-10 11:02:29 +0530

    header links verified and cliecked

---
+++ SeleniumJava/com.crm.seleniumM4/src/popUpAssignments/DWSHeaderLinksNewTab.java
@@ -0,0 +1,29 @@
+package popUpAssignments;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import utilityClasses.BaseClassDWS;
+
+public class DWSHeaderLinksNewTab extends BaseClassDWS{
+	public static void main(String[] args) {
+		useChrome();
+		
+		if(driver.findElement(By.className("rightside-3")).isDisplayed())
+			System.out.println("Page verified by element. ");
+	
+		Actions actions = new Actions(driver);
+		List<WebElement> headerLinks = driver.findElements(By.xpath("//div[@class='header-links-wrapper']/div/ul/li"));
+		System.out.println("Total elements: " + headerLinks.size());
+		for (WebElement link: headerLinks) {
+			actions.keyDown(Keys.CONTROL).click(link).perform(); sleep(1000);
+			System.out.println("clicked");
+		}
+		
+		postCondition2();
+	}
+}
