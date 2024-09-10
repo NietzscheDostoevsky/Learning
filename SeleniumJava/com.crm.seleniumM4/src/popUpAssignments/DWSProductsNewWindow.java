fbe78531cc23ff5d7ceb541c3b2d281b643a8cd7
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-10 10:51:53 +0530

    page verified

--- SeleniumJava/com.crm.seleniumM4/src/popUpAssignments/DWSProductsNewWindow.java
+++ SeleniumJava/com.crm.seleniumM4/src/popUpAssignments/DWSProductsNewWindow.java
@@ -12,6 +12,10 @@ import utilityClasses.BaseClassDWS;
 public class DWSProductsNewWindow  extends BaseClassDWS{
 	public static void main(String[] args) {
 		useChrome();
+		
+		if(driver.findElement(By.className("rightside-3")).isDisplayed())
+				System.out.println("Page verified by element. ");
+		
 		Actions actions = new Actions(driver);
 		List<WebElement> topBarElements = driver.findElements(By.xpath("//ul[@class='top-menu']/li"));
 		for (WebElement link: topBarElements) {
