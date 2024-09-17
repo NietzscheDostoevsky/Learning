40ffb1054bf3735efd25a19d78883551fdf28dcc
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-18 02:32:25 +0530

    reduce done

--- Java/javaReference/Chapter30TheStreamAPI/src/obtainStream/ReductionDemo.java
+++ Java/javaReference/Chapter30TheStreamAPI/src/obtainStream/ReductionDemo.java
@@ -23,5 +23,28 @@ public class ReductionDemo {
 		// own practice/ 
 		
 		System.out.println("Product: " + myList.stream().reduce(1, (a,b) -> a*b));
+		
+		// More examples. 
+		
+		// 1. summing integers with reduce
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+		System.out.println("Sum of numbers: " + numbers.stream().reduce(0, (a,b) -> a + b));
+		
+		// 2. Concatenating Strings with reduce
+		List<String> words = Arrays.asList("Different", "combination", "of", "words");
+		System.out.println("Combined: " + words.stream().reduce("", (a,b) -> a + " " + b));
+		
+		// 3. Finding the Maximum Value with reduce
+		numbers = Arrays.asList(5, 3, 8, 2, 9);
+		Optional<Integer> max = numbers.stream().reduce((a,b) -> a>b ? a:b); 
+		if (max.isPresent())
+			System.out.println("Max in " + numbers + " :" + max.get());
+		
+		// 4. Product of only even values. 
+		numbers = Arrays.asList(7,18,10,24,17,5);
+		System.out.println("Even product: " + numbers.stream().filter(a->a%2==0).reduce(1, (a,b) -> a*b));
+		System.out.println("Even product: " + numbers.stream().reduce(1, (a,b) -> b%2==0 ? a*b:a));
+		System.out.println("Odd  product: " + numbers.stream().reduce(1, (a,b) -> b%2 !=0 ? a*b:a));
+		System.out.println("Odd  product: " + numbers.stream().reduce(1, (a,b) -> b%2==0 ? a:a*b));
 	}
 }
