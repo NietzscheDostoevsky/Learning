fc3b2458c5d267695581c08243bd24861fe46dc6
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-18 20:18:01 +0530

    flatMap

--- Java/javaReference/Chapter30TheStreamAPI/src/mapping/FlatMap.java
+++ Java/javaReference/Chapter30TheStreamAPI/src/mapping/FlatMap.java
@@ -27,8 +27,94 @@ public class FlatMap {
 		Stream<Stream<String>> streamOfStreams = namesList.stream().map(List::stream);
 		streamOfStreams.forEach(s -> s.forEach(System.out::println));
 		System.out.println();
-		// using flatMap()
+		
+		// using flatMap(), eliminates the need to iterate over substreams. 
 		Stream<String> flatStream = namesList.stream().flatMap(List::stream);
 		flatStream.forEach(System.out::println);
+		System.out.println();
+		
+		// flatMap() example for words in sentences. 
+		// using flatMap() to split sentences into words and process each word. 
+		
+		List<String> sentences = Arrays.asList(
+				"Hello world", 
+				"How are you",
+				"Java streams are POWERFUL"
+		);
+		System.out.println(sentences.size());
+		
+		sentences.stream()
+				 .flatMap(sentence -> Arrays.stream(sentence.split(" ")))
+				 .map(word->word.toUpperCase())
+				 .forEach(System.out::println);
+		
+		// Arrays.stream(sentence.split(" ")) splits each sentence into words and 
+		//	returns a stream of words for each sentence.
+		
+		// flatMap() takes care of flattening all the individual streams of words into a single stream.
+		
+		
+		//Equivalent code as above, but according to chatGPT flatmap version is better. 
+		// And contains a nested forEach inside a forEach, which is bad practice in computing. 
+		System.out.println();
+		sentences.forEach(sentence -> {
+			Arrays.stream(sentence.split(" "))
+				  .map(word->word.toUpperCase())
+				  .forEach(System.out::println);
+		});
+		
+		
+		System.out.println();
+		// Equivalent imperetive code : 
+		for (String sentence: sentences) {
+			String[] words = sentence.split(" ");
+			for (String word: words) {
+				System.out.println(word.toLowerCase());
+			}
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
