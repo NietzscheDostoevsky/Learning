54bd3b04b84876d8a11308f56c48e30951664f21
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-19 12:37:54 +0530

    changed to work with crossplatform absolute apth

--- SeleniumJava/com.crm.seleniumM4/src/popUp/FileUploadPopup.java
+++ SeleniumJava/com.crm.seleniumM4/src/popUp/FileUploadPopup.java
@@ -1,5 +1,7 @@
 package popUp;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -9,12 +11,14 @@ public class FileUploadPopup extends BaseClassDWS {
 	public static void main(String[] args) {
 		String urlString = "https://www.ilovepdf.com/word_to_pdf";
 		useChrome(urlString);
-		String filepath = "C:\\GitHub\\Learning\\SeleniumJava\\test.docx";
-		String filepath2 = "C:/GitHub/Learning/SeleniumJava/test.docx";
+		//String filepath = "C:\\GitHub\\Learning\\SeleniumJava\\test.docx";
+		String filepath2 = "testdata/test.docx";
 		
-		
+		var file = new File(filepath2);
+		System.out.println(file.getClass().getName());
+		System.out.println(file.getAbsolutePath());
 		WebElement sendFile = driver.findElement(By.xpath("//input[@type='file']"));
-		sendFile.sendKeys(filepath);
+		sendFile.sendKeys(file.getAbsolutePath());
 		
 		sleep(3000);
 		
