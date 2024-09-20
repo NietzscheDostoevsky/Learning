11588e1c72171ce7b2d2715280c264b65eb15a2d
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-20 16:20:29 +0530

    redbus done

---
+++ SeleniumJava/com.crm.seleniumM4/src/popUpAssignments/RedBusCalender.java
@@ -0,0 +1,39 @@
+package popUpAssignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+/*
+ * Write a script for RedBus website.  
+ * Open the browser.  
+ * Maximize() 
+ * Enter into redbus.  
+ * Verify the page by using title.  
+ * After verification, fill all the details of from and to.  
+ * Can choose any details.  
+ * In date, click 27 sep and search the bus.
+ */
+
+import utilityClasses.BaseClassDWS;
+
+public class RedBusCalender extends BaseClassDWS{
+	public static void main(String[] args) {
+		String url = "https://www.redbus.in/" ;
+		
+		useChrome(url);
+		if(driver.getTitle().equals("Bus Ticket Booking Online made Easy, Secure with Top Bus Operators - redBus"))
+			System.out.println("Page verified by title");
+		
+		driver.findElement(By.id("src")).sendKeys("Pune"); sleep(2000);  
+		driver.findElement(By.id("src")).sendKeys(Keys.ENTER);
+		driver.findElement(By.id("dest")).sendKeys("Mumbai"); sleep(2000);  
+		driver.findElement(By.id("dest")).sendKeys(Keys.ENTER);
+		
+		driver.findElement(By.id("onwardCal")).click(); sleep(2000);  
+		
+		
+		// Close the browser. 
+		postCondition2();
+	}
+
+}
