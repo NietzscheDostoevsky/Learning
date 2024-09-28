627535e7e86be23c6c5b883cf1c8ad95c83b4a1d
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-28 11:29:53 +0530

    Create ScrollBasedOnWebElement.java

---
+++ SeleniumJava/com.crm.seleniumM4/src/javaScriptExecutor/ScrollBasedOnWebElement.java
@@ -0,0 +1,34 @@
+package javaScriptExecutor;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import utilityClasses.BaseClassDWS;
+
+public class ScrollBasedOnWebElement extends BaseClassDWS {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.redbus.in");
+		sleep(3000);
+		JavascriptExecutor  js = (JavascriptExecutor) driver;
+//		WebElement bookTrain =  driver.findElement(By.linkText("Book Train Tickets"));
+//		js.executeScript("arguments[0].scrollIntoView(false);", bookTrain);
+		//sleep(3000);
+		String script = """
+					window.scrollBy(0, 100);
+				""";
+		while(true) {
+			js.executeScript(script);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+		}
+	}
+}
