267cc781eb726a99e175e829b48d4d10d3b2e7f4
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-30 11:23:01 +0530

    added dependency

---
+++ SeleniumJava/com.crm.AdvanceSelenium/src/test/java/basic/LaunchTheBrowser.java
@@ -0,0 +1,12 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchTheBrowser {
+	public static void main(String[] args) {
+		//Open the browser. 
+		WebDriver driver = new ChromeDriver(); 
+		
+	}
+}
