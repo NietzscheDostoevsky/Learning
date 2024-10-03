3dbb0bb6de6dacec86844d2189de2a1f1fd6e7f2
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-03 22:42:44 +0530

    resilved errors

--- SeleniumJava/com.crm.AdvanceSelenium/src/test/java/basic/LaunchTheBrowser.java
+++ SeleniumJava/com.crm.AdvanceSelenium/src/test/java/basic/LaunchTheBrowser.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchTheBrowser {
 	public static void main(String[] args) {
 		//Open the browser. 
-		WebDriver driver = new ChromeDriver(); 
+		WebDriver driver = new ChromeDriver();
+		driver.quit();
 		
 	}
 }
