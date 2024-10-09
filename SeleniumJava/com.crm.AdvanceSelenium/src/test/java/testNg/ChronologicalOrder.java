94499836dd46e02b89a1f917712186e4d0081bb1
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-09 09:46:48 +0530

    test execution order

---
+++ SeleniumJava/com.crm.AdvanceSelenium/src/test/java/testNg/ChronologicalOrder.java
@@ -0,0 +1,37 @@
+package testNg;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ChronologicalOrder {
+	
+	@BeforeSuite
+	
+	@BeforeTest
+		
+	
+	@BeforeClass
+	
+	@BeforeMethod
+	
+	@Test
+	
+	
+	@AfterMethod
+	
+	@AfterClass
+	
+	@AfterTest
+	
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("After suite");
+	}
+}
