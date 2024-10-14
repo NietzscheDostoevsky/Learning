68bffbe67052ca35c92b7e204f68eb4f87536916
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-14 14:14:37 +0530

    changed dir

--- SeleniumJava/com.dws.test/src/main/java/com/dws/fileutility/PropertiesFile.java
+++ SeleniumJava/com.dws.test/src/main/java/com/dws/fileutility/PropertiesFile.java
@@ -8,7 +8,7 @@ public class PropertiesFile {
 	public static String property(String key) {
 		Properties prop = new Properties();
 		try {
-			FileInputStream fis = new FileInputStream("./Configure/dwsLogin.properties");
+			FileInputStream fis = new FileInputStream("./configure/dwsLogin.properties");
 			prop.load(fis);
 			return prop.getProperty(key);
 		} catch (Exception e) {
