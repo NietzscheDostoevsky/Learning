0d80fee4e5998c2ca0510f3a0f2210b7fef4506f
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-14 14:15:00 +0530

    first test success

---
+++ SeleniumJava/com.dws.test/src/test/java/DWSFollowLinks/LoginTest.java
@@ -0,0 +1,15 @@
+package DWSFollowLinks;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.dws.base.BaseClass;
+
+public class LoginTest extends BaseClass {
+	
+	@Test
+	public void loginTest() {
+		System.out.println("Loging test");
+	}
+	
+}
