a0172978569530f969aba3aa93439e3e7ccb90cf
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-15 09:29:36 +0530

    skipping test cases

--- SeleniumJava/com.crm.AdvanceSelenium/src/test/java/testNg/LaunchListerners.java
+++ SeleniumJava/com.crm.AdvanceSelenium/src/test/java/testNg/LaunchListerners.java
@@ -9,20 +9,47 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-
 @Listeners(com.crm.listeners.Sample.class)
 public class LaunchListerners {
-	
+
 	@Test
-	public void dws() throws InterruptedException {
-		
+	public void google() throws InterruptedException {
+
 		String aString = "haha";
-		String bString = "hehe";
+		String bString = "haha";
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		driver.get("https://www.google.com");		
-		Thread.sleep(2000);
+		driver.get("https://www.google.com");
+
+		driver.quit();
+		assertEquals(aString, bString);
+	}
+	
+	@Test
+	public void bing() throws InterruptedException {
+
+		String aString = "haha";
+		String bString = "hahe";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.bing.com");
+
+		driver.quit();
+		assertEquals(aString, bString);
+	}
+	
+	@Test(dependsOnMethods = "bing")
+	public void duckduckgo() throws InterruptedException {
+
+		String aString = "haha";
+		String bString = "haha";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://duckduckgo.com/");
+
 		driver.quit();
 		assertEquals(aString, bString);
 	}
