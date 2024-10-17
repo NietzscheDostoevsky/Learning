00b1a1e0d0e2635b786da84dbf846d26bb593d76
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-17 08:48:35 +0530

    success

--- VTigerCRM/com.crm.vtiger600/src/test/java/setup/Setup.java
+++ VTigerCRM/com.crm.vtiger600/src/test/java/setup/Setup.java
@@ -1,29 +1,51 @@
 package setup;
 
-import java.time.Duration;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 import static com.crm.utility.PropertiesFile.property;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Setup {
-	
-	WebDriver driver ;
-	
+
+	WebDriver driver;
+
 	@BeforeClass
 	public void openBrowser() {
-		driver = new ChromeDriver(); 
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get(property("url"));
-		
 	}
-	
-	
+
+	@BeforeMethod
+	public void login() {
+		driver.findElement(By.name("user_name")).sendKeys(property("username"));
+		driver.findElement(By.name("user_password")).sendKeys(property("password"));
+		driver.findElement(By.id("submitButton")).click();
+	}
+
 	@Test
 	public void setup() {
-		
+		System.out.println("Login successful");
 	}
+	
+	@AfterMethod
+	public void logout()  {
+		driver.findElement(By.xpath("(//td[@class='small'])[2]")).click();
+		driver.findElement(By.xpath("(//a[@class='drop_down_usersettings'])[2]")).click();
+	}
+	
+	@AfterClass
+	public void postCondition() {
+		driver.quit();
+	}
+
 }
