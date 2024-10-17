73fa747cd95bbe9ed5cefd58cab3ff81fb39b36d
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-17 08:49:11 +0530

    baseclass working

--- VTigerCRM/com.crm.vtiger600/src/main/java/com/crm/baseclass/VtigerBaseClass.java
+++ VTigerCRM/com.crm.vtiger600/src/main/java/com/crm/baseclass/VtigerBaseClass.java
@@ -1,12 +1,16 @@
 package com.crm.baseclass;
 
-import java.time.Duration;
 import static com.crm.utility.PropertiesFile.property;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 public class VtigerBaseClass {
 	
@@ -20,8 +24,22 @@ public class VtigerBaseClass {
 		driver.get(property("url"));
 	}
 	
+	@BeforeMethod
+	public void login() {
+		driver.findElement(By.name("user_name")).sendKeys(property("username"));
+		driver.findElement(By.name("user_password")).sendKeys(property("password"));
+		driver.findElement(By.id("submitButton")).click();
+	}
+	
+	@AfterMethod
+	public void logout()  {
+		driver.findElement(By.xpath("(//td[@class='small'])[2]")).click();
+		driver.findElement(By.xpath("(//a[@class='drop_down_usersettings'])[2]")).click();
+	}
+	
 	@AfterClass
 	public void postCondition() {
 		driver.quit();
 	}
+
 }
