4e2ce70c0b1c752d64dd2a62b29fbc437cb9a6ad
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-17 15:30:46 +0530

    generate random salutatino

--- VTigerCRM/com.crm.vtiger600/src/test/java/contacts/FillContactInformation.java
+++ VTigerCRM/com.crm.vtiger600/src/test/java/contacts/FillContactInformation.java
@@ -1,9 +1,43 @@
 package contacts;
 
+import java.util.Locale;
+import java.util.Random;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import net.datafaker.Faker;
 
 public class FillContactInformation {
+	
+
+	@SuppressWarnings({ "deprecation" })
 	public static void fillContactInformation(WebDriver driver) {
 		
+		// Use datafaker to generate fake data. 
+		Locale IND = new Locale("en", "IN");
+		Faker faker = new Faker(IND); 
+		
+		Select select = new Select(driver.findElement(By.name("salutationtype")));
+		select.selectByValue(randomSalutation());
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		fillContactInformation(driver); 
+		driver.quit();
+	}
+	
+	public static String randomSalutation() {
+		String[] salutations = {
+				"Mr.","Mrs.","Ms.","Dr.","Prof."
+		};
+		Random r = new Random(); 
+		return salutations[r.nextInt(salutations.length)];
 	}
 }
