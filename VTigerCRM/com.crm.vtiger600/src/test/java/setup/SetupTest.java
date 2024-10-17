07f5d97e1eab070237afec770c0ca29ce22658c5
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-17 08:49:49 +0530

    test setup success

---
+++ VTigerCRM/com.crm.vtiger600/src/test/java/setup/SetupTest.java
@@ -0,0 +1,13 @@
+package setup;
+
+import org.testng.annotations.Test;
+
+import com.crm.baseclass.VtigerBaseClass;
+
+public class SetupTest extends VtigerBaseClass {
+	
+	@Test
+	public void loginLogout() {
+		
+	}
+}
