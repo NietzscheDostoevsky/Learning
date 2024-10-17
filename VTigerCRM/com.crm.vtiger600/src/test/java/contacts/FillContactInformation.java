45760022461b5b02ab292704f7657f4ae233fbbc
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-17 16:32:50 +0530

    added organization name from window

--- VTigerCRM/com.crm.vtiger600/src/test/java/contacts/FillContactInformation.java
+++ VTigerCRM/com.crm.vtiger600/src/test/java/contacts/FillContactInformation.java
@@ -1,11 +1,12 @@
 package contacts;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import net.datafaker.Faker;
@@ -14,23 +15,21 @@ public class FillContactInformation {
 	
 
 	@SuppressWarnings({ "deprecation" })
-	public static void fillContactInformation(WebDriver driver) {
+	public static void fillContactInformation(WebDriver driver) throws InterruptedException {
 		
 		// Use datafaker to generate fake data. 
 		Locale IND = new Locale("en", "IN");
 		Faker faker = new Faker(IND); 
 		
-		Select select = new Select(driver.findElement(By.name("salutationtype")));
-		select.selectByValue(randomSalutation());
+		new Select(driver.findElement(By.name("salutationtype")))
+			.selectByValue(randomSalutation());
 		
+		driver.findElement(By.name("firstname")).sendKeys(faker.name().firstName());
+		driver.findElement(By.name("lastname")).sendKeys(faker.name().lastName());
 		
+		fillOrganizationName(driver);
 		
-	}
-	
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		fillContactInformation(driver); 
-		driver.quit();
+		driver.findElement(By.id("email")).sendKeys(faker.internet().emailAddress());
 	}
 	
 	public static String randomSalutation() {
@@ -39,5 +38,15 @@ public class FillContactInformation {
 		};
 		Random r = new Random(); 
 		return salutations[r.nextInt(salutations.length)];
+	}
+	
+	private static void fillOrganizationName(WebDriver driver) throws InterruptedException {
+		String parentWindow = driver.getWindowHandle();
+		driver.findElement(By.xpath("(//img[@title='Select'])[1]")).click();
+		List<String> windows = new LinkedList<String>(driver.getWindowHandles());
+		driver.switchTo().window(windows.getLast());
+		driver.findElement(By.id("search_txt")).sendKeys("its working");
+		driver.findElement(By.xpath("(//tbody/tr[@bgcolor='white'])[1]/td/a")).click();
+		driver.switchTo().window(parentWindow);
 	}
 }
