7dfd8384307419e38a96b589c5548ef5cedb69a2
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-21 16:48:16 +0530

    Create DWSListerner.java

---
+++ SeleniumJava/com.crm.AdvanceSelenium/src/main/java/com/crm/listeners/DWSListerner.java
@@ -0,0 +1,55 @@
+package com.crm.listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class DWSListerner implements ITestListener {
+	public static ExtentSparkReporter spark;
+	public static ExtentReports report;
+	public static ExtentTest test; 
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		String name = result.getMethod().getMethodName();
+		test = report.createTest(name);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) { }
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) { }
+
+	@Override
+	public void onStart(ITestContext context) {
+		spark = new ExtentSparkReporter("./ExtentReport/DWSExtentReport.html");
+		spark.config().setDocumentTitle("Functinoal Testing");
+		spark.config().setReportName("Saurabh");
+		spark.config().setTheme(Theme.STANDARD);
+		report = new ExtentReports();
+		report.setSystemInfo("os", "windows-11");
+		report.setSystemInfo("browser", "chrome 101");
+		report.attachReporter(spark);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		report.flush();
+	}
+	
+}
