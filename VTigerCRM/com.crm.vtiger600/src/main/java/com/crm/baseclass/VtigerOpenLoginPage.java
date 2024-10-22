1b8a0268f4cb4fdca5ee0792987e81b8715bc244
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-22 21:18:40 +0530

    Create VtigerOpenLoginPage.java

---
+++ VTigerCRM/com.crm.vtiger600/src/main/java/com/crm/baseclass/VtigerOpenLoginPage.java
@@ -0,0 +1,35 @@
+package com.crm.baseclass;
+
+import static com.crm.utility.PropertiesFile.property;
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public class VtigerOpenLoginPage {
+	protected WebDriver driver;
+
+	@BeforeClass
+	public void preCondition() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(property("url"));
+	}
+
+	@BeforeMethod
+	public void verifyLoginPage() {
+		assertEquals(driver.getTitle(), "vtiger CRM 5 - Commercial Open Source CRM" );
+	}
+
+
+	@AfterClass
+	public void postCondition() {
+		driver.quit();
+	}
+}
