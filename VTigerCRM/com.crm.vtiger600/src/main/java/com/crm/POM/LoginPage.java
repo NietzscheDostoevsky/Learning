0eb0803db3d277d66a278f347eebba3c73e22c88
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-22 21:11:10 +0530

    login page DOM model

--- VTigerCRM/com.crm.vtiger600/src/main/java/com/crm/POM/LoginPage.java
+++ VTigerCRM/com.crm.vtiger600/src/main/java/com/crm/POM/LoginPage.java
@@ -30,8 +30,28 @@ public class LoginPage {
 	@FindBy(xpath = "//img[@alt='vtiger Android Application']/..")
 	public WebElement androidAppLink;
 	
-	//Connect with us buttons
+	//Connect with us links
 	
+	@FindBy(xpath = "//img[@alt='Facebook']/.." )
+	public WebElement facebookPage; 
+	
+	@FindBy(xpath = "//img[@alt='Twitter']/.." )
+	public WebElement TwitterPage; 
+	
+	@FindBy(xpath = "//img[@alt='Linkedin']/.." )
+	public WebElement LinkedinPage; 
+	
+	@FindBy(xpath = "//img[@alt='Videos']/.." )
+	public WebElement YoutubePage; 
+	
+	@FindBy(xpath = "//img[@alt='Manuals']/.." )
+	public WebElement VtigerWikiPage; 
+	
+	@FindBy(xpath = "//img[@alt='Forums']/.." )
+	public WebElement VtigerForumPage; 
+	
+	@FindBy(xpath = "//img[@alt='Blogs']/.." )
+	public WebElement VtigerBlogPage; 
 	
 	//Constructor
 	public LoginPage(WebDriver driver) {
