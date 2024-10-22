178771de10b14845fb9f8dfffece3266f670cb36
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-22 21:33:21 +0530

    first etst case passed

--- VTigerCRM/com.crm.vtiger600/src/test/java/loginPageTestScenarios/ElementsDisplayed.java
+++ VTigerCRM/com.crm.vtiger600/src/test/java/loginPageTestScenarios/ElementsDisplayed.java
@@ -1,5 +1,17 @@
 package loginPageTestScenarios;
 
-public class ElementsDisplayed {
+import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.Test;
+
+import com.crm.baseclass.VtigerOpenLoginPage;
+
+public class ElementsDisplayed extends VtigerOpenLoginPage {
+	
+	
+	@Test
+	public void verifyOutlook() {
+		assertEquals(ref.outlookPlugin.isDisplayed(), true);
+	}
+	
 }
