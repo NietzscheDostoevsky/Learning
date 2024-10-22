88211375a0a9ec7b8bfec7b29d62649967314ff4
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-22 21:45:42 +0530

    element displayed test cases

--- VTigerCRM/com.crm.vtiger600/src/test/java/loginPageTestScenarios/ElementsDisplayed.java
+++ VTigerCRM/com.crm.vtiger600/src/test/java/loginPageTestScenarios/ElementsDisplayed.java
@@ -14,4 +14,48 @@ public class ElementsDisplayed extends VtigerOpenLoginPage {
 		assertEquals(ref.outlookPlugin.isDisplayed(), true);
 	}
 	
+	@Test
+	public void verifyExchangeConnector() {
+		assertEquals(ref.exchangeConnector.isDisplayed(), true);
+	}
+	
+	@Test
+	public void verifyIPhoneAppLink() {
+		assertEquals(ref.iPhoneAppLink.isDisplayed(), true);
+	}
+	
+	@Test 
+	public void verifyAndroidAppLink() {
+		assertEquals(ref.androidAppLink.isDisplayed(), true);
+	}
+	
+	@Test
+	public void verifyFacebookPageLink() {
+		assertEquals(ref.facebookPage.isDisplayed(), true); 
+	}
+	
+	@Test
+	public void verifyTwitterLink() {
+		assertEquals(ref.TwitterPage.isDisplayed(), true);
+	}
+	@Test
+	public void verifyLinkedinLink(){
+		assertEquals(ref.LinkedinPage.isDisplayed(), true);
+	}
+	@Test
+	public void verifyYoutubeLink() {
+		assertEquals(ref.YoutubePage.isDisplayed(), true);
+	}
+	@Test
+	public void verifyVtigerWikiLink() {
+		assertEquals(ref.VtigerWikiPage.isDisplayed(), true);
+	}
+	@Test
+	public void  verifyVtigerForumLink(){
+		assertEquals(ref.VtigerForumPage, true);
+	}
+	@Test
+	public void verifyVtigerBlogLink(){
+		assertEquals(ref.VtigerBlogPage, true);
+	}
 }
