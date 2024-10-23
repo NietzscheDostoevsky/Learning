6607e5bc68a4edb4e406379882eee0170e95c48b
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-24 00:52:23 +0530

    added docs

--- VTigerCRM/com.crm.vtiger600/src/main/java/com/crm/baseclass/VtigerLoginFollowUsLinksBaseClass.java
+++ VTigerCRM/com.crm.vtiger600/src/main/java/com/crm/baseclass/VtigerLoginFollowUsLinksBaseClass.java
@@ -1,5 +1,10 @@
 package com.crm.baseclass;
 
+/*
+ * Opens the vtiger login page for testing. 
+ * creates a POM reference named ref of the login page.
+ * provides base functionality to test follow us links on the login page. 
+ */
 public class VtigerLoginFollowUsLinksBaseClass {
 
 }
