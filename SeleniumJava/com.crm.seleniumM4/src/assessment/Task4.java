30b70ca7524814418db50d7dd884db9433904a82
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-25 16:06:11 +0530

    Update Task4.java

--- SeleniumJava/com.crm.seleniumM4/src/assessment/Task4.java
+++ SeleniumJava/com.crm.seleniumM4/src/assessment/Task4.java
@@ -1,5 +1,26 @@
 package assessment;
 
-public class Task4 {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class Task4 extends DWSBaseClass {
+	public static void main(String[] args) {
+		createDriver("chrome");
+		dwsLogin();
+		
+		driver.findElement(By.xpath("//ul[@class='top-menu']/li[5]")).click();
+		
+		Select sortSelect = new Select(driver.findElement(By.id("products-orderby")));
+		List<WebElement> sortOptions = sortSelect.getOptions();
+		int index = 0; 
+		for (WebElement element : sortOptions) {
+			sortSelect.selectByIndex(index++);
+			sortSelect = new Select(driver.findElement(By.id("products-orderby")));
+		}
+		dwsLogout();
+		postCondition();
+	}
 }
