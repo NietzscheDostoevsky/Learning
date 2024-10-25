1c2c3b265f787c9ff7ea91eed42defdf524e2105
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-10-25 23:06:11 +0530

    updated testing login to use excel

--- VTigerCRM/com.crm.vtiger600/src/test/java/loginPageTestScenarios/LoginFunctionality.java
+++ VTigerCRM/com.crm.vtiger600/src/test/java/loginPageTestScenarios/LoginFunctionality.java
@@ -1,5 +1,6 @@
 package loginPageTestScenarios;
 
+import static com.crm.utility.LoginFunctionalityExcelFile.excel;
 import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.DataProvider;
@@ -19,6 +20,7 @@ public class LoginFunctionality extends VtigerTestLoginFunctionBaseClass {
 
 	@Test(dataProvider = "NegativeLogin")
 	public void testLoginNegative(String username, String password) {
+		System.out.println("Testing with login details : " + username + " " + password);
 		ref.loginTextfield.sendKeys(username);
 		ref.passwordTextfield.sendKeys(password);
 		ref.loginButton.click();
@@ -28,17 +30,21 @@ public class LoginFunctionality extends VtigerTestLoginFunctionBaseClass {
 	@DataProvider(name = "PositiveLogin")
 	public Object[][] positiveSender() {
 		Object[][] obj = new Object[1][2];
-		obj[0][0] = "admin";
-		obj[0][1] = "root";
+		obj[0][0] = excel(2, 7);
+		obj[0][1] = excel(2, 8);
 
 		return obj;
 	}
 
 	@DataProvider(name = "NegativeLogin")
 	public Object[][] negativeSender() {
-		Object[][] obj = new Object[1][2];
-		obj[0][0] = "pewpew";
-		obj[0][1] = "haha";
+		int testcases = 7; 
+		int startRow = 3; // 0 index
+		int startCol = 7; // 0 index
+		Object[][] obj = new Object[testcases][2];
+		for (int i = 0; i <  testcases ; i++)
+			for (int j = 0; j < 2 ; j++)
+				obj[i][j] = excel(i + startRow, j + startCol); 
 
 		return obj;
 	}
