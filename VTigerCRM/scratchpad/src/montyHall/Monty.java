5a7d79921ac44d52e1a3ce817f98ff3a5eb0dca7
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-11-05 20:52:26 +0530

    Create Monty.java

---
+++ VTigerCRM/scratchpad/src/montyHall/Monty.java
@@ -0,0 +1,20 @@
+package montyHall;
+
+/*
+ * Demostrate the simulation of Monty Hall problem
+ * Where the decision is to switch the door or not
+ * chosen initially. 
+ */
+
+public class Monty {
+
+	public static void out(String out) {
+		System.out.print(out);
+	}
+	
+	public static void play(boolean switchDoor) {
+		
+		// Hide the car behind one of the three doors chosen randomly 
+	}
+	
+}
