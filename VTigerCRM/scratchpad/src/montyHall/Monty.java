45b86b9e49f92b743c86400b6552dc75b5657cdb
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-11-05 21:07:12 +0530

    door choses

--- VTigerCRM/scratchpad/src/montyHall/Monty.java
+++ VTigerCRM/scratchpad/src/montyHall/Monty.java
@@ -1,5 +1,11 @@
 package montyHall;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 /*
  * Demostrate the simulation of Monty Hall problem
  * Where the decision is to switch the door or not
@@ -13,8 +19,16 @@ public class Monty {
 	}
 	
 	public static void play(boolean switchDoor) {
-		
 		// Hide the car behind one of the three doors chosen randomly 
+		
+		List<String> doorStrings =new  ArrayList<String>(Arrays.asList("car", "goat","goat")); 
+		Collections.shuffle(doorStrings);
+		
+		// Let the player make their first guess and choose their door. 
+		int[] doors = {1,2,3};
+		int chosenDoor =  doors[new Random().nextInt(doors.length)];
+		out("Player chooses door " + chosenDoor);
+		
 	}
 	
 }
